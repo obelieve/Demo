7c724a4ea33dbd8b61ae5d3ddf4ecb0f1898aff9
zxy <798486005@qq.com>
2018-08-17 09:36:17 +0800

    设定索引 可以使用unique指定属性值是否唯一

--- app/src/main/java/com/zxy/demo/User.java
+++ app/src/main/java/com/zxy/demo/User.java
@@ -3,13 +3,14 @@ package com.zxy.demo;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 import android.graphics.Bitmap;
 
 /**
  * Created by zxy on 2018/8/17 09:25.
  */
-@Entity(tableName = "table_user")
+@Entity(tableName = "table_user",indices = {@Index("name"),@Index(value = {"name","age"},unique = true)})
 public class User
 {
     @PrimaryKey(autoGenerate = true)
