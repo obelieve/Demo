d2fff5131c5ed4ebc71c839ef012ae44629dd8bb
zxy <798486005@qq.com>
2018-08-17 09:32:04 +0800

    可以添加表和属性的别名

--- app/src/main/java/com/zxy/demo/User.java
+++ app/src/main/java/com/zxy/demo/User.java
@@ -1,19 +1,21 @@
 package com.zxy.demo;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
 /**
  * Created by zxy on 2018/8/17 09:25.
  */
-@Entity
+@Entity(tableName = "table_user")
 public class User
 {
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "_id")
     private int id;
-
+    @ColumnInfo(name = "name")
     private String name;
-
+    @ColumnInfo(name = "age")
     private int age;
 
     public int getId()
