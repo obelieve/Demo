aafee9683f51f1b85b47f21f7ed153c0f99c955a
zxy <798486005@qq.com>
2018-08-17 09:46:20 +0800

    添加外键方式

---
+++ app/src/main/java/com/zxy/demo/Book.java
@@ -0,0 +1,64 @@
+package com.zxy.demo;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by zxy on 2018/8/17 09:42.
+ */
+//外键 user_id 默认支持级联删除和更新
+@Entity(foreignKeys = {@ForeignKey(entity = User.class,parentColumns = "id",childColumns = "user_id")})
+public class Book
+{
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String name;
+
+    private String type;
+
+    @ColumnInfo(name = "user_id")
+    private int userId;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+}
