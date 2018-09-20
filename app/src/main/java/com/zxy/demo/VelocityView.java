03d0b8f635dcdc6741ac2849077fc8ec5849f4fb
zxy <798486005@qq.com>
2018-09-20 23:59:41 +0800

    u

--- app/src/main/java/com/zxy/demo/VelocityView.java
+++ app/src/main/java/com/zxy/demo/VelocityView.java
@@ -13,31 +13,27 @@ import com.zxy.utility.LogUtil;
  * Created by zxy on 2018/9/20 17:24.
  */
 
-public class VelocityView extends View
-{
+public class VelocityView extends View {
     VelocityTracker mVelocityTracker;
 
-    public VelocityView(Context context, @Nullable AttributeSet attrs)
-    {
+    public VelocityView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
-    long start = 0,end;
-    float sX = 0,eX;
+
+    long start = 0, end;
+    float sX = 0, eX;
+
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
+    public boolean onTouchEvent(MotionEvent event) {
         LogUtil.e(MotionEventUtil.name(event));
 
-        switch (event.getAction())
-        {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                mVelocityTracker = VelocityTracker.obtain();
-                if(mVelocityTracker==null){
+                if (mVelocityTracker == null) {
                     mVelocityTracker = VelocityTracker.obtain();
+                } else {
+                    mVelocityTracker.clear();
                 }
-//                else{
-//                    mVelocityTracker.clear();
-//                }
                 mVelocityTracker.addMovement(event);
                 start = System.currentTimeMillis();
                 sX = event.getX();
@@ -45,15 +41,16 @@ public class VelocityView extends View
             case MotionEvent.ACTION_MOVE:
                 mVelocityTracker.addMovement(event);
                 mVelocityTracker.computeCurrentVelocity(100);
-                LogUtil.e("VelocityTracker x:"+mVelocityTracker.getXVelocity());
-                LogUtil.e("VelocityTracker y:"+mVelocityTracker.getYVelocity());
+                LogUtil.e("VelocityTracker x:" + mVelocityTracker.getXVelocity());
+                LogUtil.e("VelocityTracker y:" + mVelocityTracker.getYVelocity());
                 end = (System.currentTimeMillis() - start);
-                eX = (event.getX()-sX);
-                LogUtil.e(start+"耗时 ms："+end+" eX:"+eX);
+                eX = (event.getX() - sX);
+                LogUtil.e(start + "耗时 ms：" + end + " eX:" + eX);
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                mVelocityTracker.clear();
+                mVelocityTracker.recycle();//调用这个方法意味着释放了这个类的资源，无法再次调用recycle
+                mVelocityTracker = null;//需要设为null
                 break;
         }
         return true;
