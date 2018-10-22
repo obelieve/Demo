090febab0bd1faa84ead666a3a8e1300f7f96601
zxy <798486005@qq.com>
2018-10-22 16:35:06 +0800

    1.ViewGroup 有设置android:background时才会调用onDraw;2.scrollTo会调用invalidate() 进而调用computeScroll()

--- app/src/main/java/com/zxy/view_computescroll/SView.java
+++ app/src/main/java/com/zxy/view_computescroll/SView.java
@@ -1,10 +1,12 @@
 package com.zxy.view_computescroll;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+import android.widget.Scroller;
 
 import com.zxy.utility.LogUtil;
 
@@ -14,6 +16,12 @@ import com.zxy.utility.LogUtil;
 
 public class SView extends FrameLayout
 {
+    Scroller mScroller;
+
+    {
+        mScroller = new Scroller(getContext());
+    }
+
     public SView(@NonNull Context context)
     {
         super(context);
@@ -28,6 +36,42 @@ public class SView extends FrameLayout
     public void computeScroll()
     {
         super.computeScroll();
+        LogUtil.e();
+
+        if (mScroller.computeScrollOffset())
+        {
+            LogUtil.e("computeScrollOffset()");
+            scrollTo(0, mScroller.getCurrY());
+//            int[] ints = new int[]{Color.BLACK, Color.WHITE, Color.BLUE, Color.RED, Color.LTGRAY};
+//            setBackgroundColor(ints[new Random().nextInt(ints.length)]);//设置背景 onDraw()会调用
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+        LogUtil.e();
+    }
+
+    @Override
+    public void invalidate()
+    {
+        super.invalidate();
+        LogUtil.e();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        LogUtil.e();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom)
+    {
+        super.onLayout(changed, left, top, right, bottom);
         LogUtil.e();
     }
 }
--- app/src/main/java/com/zxy/view_computescroll/ScrollActivity.java
+++ app/src/main/java/com/zxy/view_computescroll/ScrollActivity.java
@@ -2,11 +2,13 @@ package com.zxy.view_computescroll;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.zxy.demo.R;
+import com.zxy.utility.LogUtil;
 
 /**
  * Created by zxy on 2018/10/20 17:35.
@@ -16,13 +18,14 @@ public class ScrollActivity extends Activity
 {
 
     ListView lv;
-
+    SView sv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll);
         lv = (ListView) findViewById(R.id.lv);
+        sv = (SView) findViewById(R.id.sv);
         String[] s = new String[30];
         int i = 0;
         while (i < 30)
@@ -31,5 +34,17 @@ public class ScrollActivity extends Activity
             i++;
         }
         lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, s));
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                LogUtil.e("handler->执行");
+                sv.mScroller.startScroll(0, 0, 0, -200,600);
+                sv.invalidate();
+
+//                ViewCompat.offsetTopAndBottom(findViewById(R.id.lv),200);
+            }
+        },2000);
     }
 }
