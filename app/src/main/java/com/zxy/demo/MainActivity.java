58ad880fb6a5aef768496472eb0638916d4f924b
zxy <798486005@qq.com>
2018-10-29 18:37:17 +0800

    1.添加底部导航控件

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -1,15 +1,18 @@
 package com.zxy.demo;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
 
+    BottomNavigationView bnv_view;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bnv_view = findViewById(R.id.bnv_view);
     }
 }
