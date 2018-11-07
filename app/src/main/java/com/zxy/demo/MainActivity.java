17ff2399ed50b6969c4a9b5271bf3f4136c22495
zxy <798486005@qq.com>
2018-11-07 15:19:56 +0800

    update

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -2,8 +2,6 @@ package com.zxy.demo;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,33 +26,7 @@ public class MainActivity extends AppCompatActivity {
         ivp_content.setAdapter(mPagerAdapter);
         ivp_content.setOffscreenPageLimit(3);
         ivp_content.setPageSpan(30);
-        ivp_content.setCurrentItem(1);
-        ivp_content.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                if (position == 0) {
-                    ivp_content.setCurrentItem(mPagerAdapter.getCount() - 1 - 1, false);
-                } else if (position == mPagerAdapter.getCount() - 1) {
-                    ivp_content.setCurrentItem(0 + 1, false);
-//                    ivp_content.postDelayed(new Runnable() {
-//                        @Override
-//                        public void run() {
-//
-//                        }
-//                    },200);
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
+        ivp_content.setCurrentItem(0);
     }
 
     public class PagerAdapter extends android.support.v4.view.PagerAdapter {
@@ -63,20 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
         {
 
-            View sview = LayoutInflater.from(getBaseContext()).inflate(R.layout.adapter_item, null);
-            ImageView siv = (ImageView) sview.findViewById(R.id.iv);
-            siv.setImageResource(mInts[mInts.length - 1]);
-            mList.add(sview);
             for (int i = 0; i < 4; i++) {
                 View view = LayoutInflater.from(getBaseContext()).inflate(R.layout.adapter_item, null);
                 ImageView iv = (ImageView) view.findViewById(R.id.iv);
                 iv.setImageResource(mInts[(i % mInts.length)]);
                 mList.add(view);
             }
-            View eview = LayoutInflater.from(getBaseContext()).inflate(R.layout.adapter_item, null);
-            ImageView eiv = (ImageView) eview.findViewById(R.id.iv);
-            eiv.setImageResource(mInts[0]);
-            mList.add(eview);
         }
 
         @Override
