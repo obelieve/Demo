d2751a694dff8d36516434f1f8b676b0373ef3f8
zxy <798486005@qq.com>
2018-11-18 18:11:03 +0800

    合并selectManage

--- app/src/main/java/com/zxy/demo/data_struct/SelectionManage.java
+++ app/src/main/java/com/zxy/demo/data_struct/SelectionManage.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public class SelectionManage {
 
-    private Mode mMode = Mode.SELECT_SINGLE_MUST_ONE;
-    private int mCurrentIndex = -1;
+    private Mode mMode = Mode.SINGLE_MUST_ONE;
+    private List<Integer> mCurrentIndexes = new ArrayList<>();
 
     private List<View> mViews = new ArrayList<>();
     private List<Boolean> mBooleans = new ArrayList<>();
@@ -34,66 +34,96 @@ public class SelectionManage {
     public void setCurrentItem(int index) {
         if (index >= 0 && index < mViews.size()) {
             switch (mMode) {
-                case SELECT_SINGLE:
-                    if (mCurrentIndex != index) {
-                        setLastSelect(mCurrentIndex, false);
-                        setCurrentSelect(index, true);
+                case SINGLE:
+                    int lastIndex = mCurrentIndexes.size() > 0 ? mCurrentIndexes.get(0) : -1;
+                    if (index != lastIndex) {
+                        setNormalItem(lastIndex);
+                        setSelectedItem(index);
                     } else {
-                        if (mBooleans.get(mCurrentIndex) == true) {
-                            setCurrentSelect(mCurrentIndex, false);
-                        } else {
-                            setCurrentSelect(mCurrentIndex, true);
-                        }
+                        setNormalItem(index);
                     }
                     break;
-                case SELECT_SINGLE_MUST_ONE:
-                    if (mCurrentIndex != index) {
-                        setLastSelect(mCurrentIndex, false);
-                        setCurrentSelect(index, true);
+                case SINGLE_MUST_ONE:
+                    int lastIndex_must = mCurrentIndexes.size() > 0 ? mCurrentIndexes.get(0) : -1;
+                    if (index != lastIndex_must) {
+                        setNormalItem(lastIndex_must);
+                        setSelectedItem(index);
+                    }
+                    break;
+                case MULTI:
+                    if (mCurrentIndexes.contains(index)) {
+                        setNormalItem(index);
+                    } else {
+                        setSelectedItem(index);
+                    }
+                    break;
+                case MULTI_MUST_ONE:
+                    int size = mCurrentIndexes.size();
+                    boolean contain = mCurrentIndexes.contains(index);
+                    if (size == 0 || (size > 0 && !contain)) {
+                        setSelectedItem(index);
+                    } else if (size > 1 && contain) {
+                        setNormalItem(index);
                     }
                     break;
             }
         }
     }
 
+    private void setNormalItem(int index) {
+        if (index == -1)
+            return;
+        mBooleans.set(index, false);
+        mViews.get(index).setSelected(false);
+        mCurrentIndexes.remove((Integer) index);
+        if (mOnSelectChangeListener != null) {
+            mOnSelectChangeListener.onSelectChange(index, mViews.get(index), false);
+        }
+    }
+
+    private void setSelectedItem(int index) {
+        if (index == -1)
+            return;
+        mBooleans.set(index, true);
+        mViews.get(index).setSelected(true);
+        mCurrentIndexes.add(index);
+        if (mOnSelectChangeListener != null) {
+            mOnSelectChangeListener.onSelectChange(index, mViews.get(index), true);
+        }
+    }
+
+
     public void setCurrentItem(View view) {
         setCurrentItem(mViews.indexOf(view));
     }
 
-    private void setCurrentSelect(int index, boolean select) {
-        mBooleans.set(index, select);
-        mViews.get(index).setSelected(select);
-        switch (mMode) {
-            case SELECT_SINGLE:
-            case SELECT_SINGLE_MUST_ONE:
-                mCurrentIndex = index;
-                break;
-        }
-        if (mOnSelectChangeListener != null) {
-            mOnSelectChangeListener.onSelected(index, mViews.get(index), select);
-        }
+    public List<Integer> getSelectedIndexes() {
+        return mCurrentIndexes;
     }
 
-    private void setLastSelect(int index, boolean select) {
-        if (index != -1) {
-            mBooleans.set(index, select);
-            mViews.get(index).setSelected(select);
+    public List<View> getSelectedViews() {
+        List<View> list = new ArrayList<>();
+        for (Integer i : mCurrentIndexes) {
+            list.add(mViews.get(i));
         }
+        return list;
     }
 
     private void reset() {
-        mCurrentIndex = -1;
+        mCurrentIndexes.clear();
         mViews.clear();
         mBooleans.clear();
     }
 
     public interface OnSelectChangeListener {
 
-        void onSelected(int index, View view, boolean select);
+        void onSelectChange(int index, View view, boolean select);
     }
 
     public enum Mode {
-        SELECT_SINGLE,
-        SELECT_SINGLE_MUST_ONE
+        SINGLE,
+        SINGLE_MUST_ONE,
+        MULTI,
+        MULTI_MUST_ONE
     }
 }
