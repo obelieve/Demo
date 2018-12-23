f89c56154e4d1d0ec99f6f7a0b736c8078b2ec85
zxy <798486005@qq.com>
2018-12-23 21:56:52 +0800

    RecyclerView ItemDecoration 实现分割线效果；

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -1,15 +1,143 @@
 package com.zxy.demo;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+import com.zxy.utility.SystemInfoUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rv_content;
+    OKRecycleAdapter mAdapter;
+
+    List<String> mList = new ArrayList<String>();
+
+    public int dp2px(int size) {
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return (int) (dm.density * size);
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rv_content = (RecyclerView) findViewById(R.id.rv_content);
+        rv_content.setLayoutManager(new LinearLayoutManager(this));
+        rv_content.addItemDecoration(new RecyclerView.ItemDecoration() {
+            Paint mPaint = new Paint();
+
+            {
+                mPaint.setColor(Color.DKGRAY);
+                mPaint.setStrokeWidth(30.0f);
+            }
+
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                super.onDraw(c, parent, state);
+                for (int i = 0; i < parent.getChildCount(); i++) {
+                    //dp2px(46)
+                    c.drawRect(0, parent.getChildAt(i).getTop() - 90, parent.getChildAt(i).getRight(), parent.getChildAt(i).getTop(), mPaint);
+                }
+            }
+
+            @Override
+            public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                super.onDrawOver(c, parent, state);
+                Paint paint = new Paint();
+                paint.setColor(Color.parseColor("#aaaa0000"));
+                View view = parent.getChildAt(0);
+                if (view.getBottom() <= 90) {
+                    c.drawRect(0, 0, view.getRight(), view.getBottom(), paint);
+                } else {
+                    c.drawRect(0, 0, view.getRight(), 90, paint);
+                }
+            }
+
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.top = 90;
+            }
+        });
+        mAdapter = new OKRecycleAdapter();
+        for (int i = 0; i < 20; i++) {
+            mList.add("i:" + i);
+        }
+        mAdapter.setData(mList);
+        rv_content.setAdapter(mAdapter);
+    }
+
+    public static class OKRecycleAdapter extends RecyclerAdapter<String> {
+
+        @Override
+        public int createViewHolder() {
+            return R.layout.item_main;
+        }
+
+        @Override
+        public void bindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position, String s) {
+            TextView tv = holder.itemView.findViewById(R.id.tv_name);
+            tv.setText(s);
+        }
+    }
+
+    public static abstract class RecyclerAdapter<T> extends RecyclerView.Adapter {
+
+        private List<T> mList = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+            View view = LayoutInflater.from(parent.getContext()).inflate(createViewHolder(), parent, false);
+            return new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            bindViewHolder(holder, position, mList.get(position));
+        }
+
+        public abstract int createViewHolder();
+
+        public abstract void bindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position, T t);
+
+        @Override
+        public int getItemCount() {
+            return mList.size();
+        }
+
+        public void setData(List<T> list) {
+            if (list == null) {
+                list = new ArrayList<>();
+            }
+            mList = list;
+            notifyDataSetChanged();
+        }
+
+        public List<T> getList() {
+            return mList;
+        }
     }
 }
