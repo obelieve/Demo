affad33ed1029a9ee7d8f098851a7be88bb27f56
zxy <798486005@qq.com>
2019-03-21 08:41:20 +0800

    GradientView update

--- app/src/main/java/com/zxy/view_gradient/GradientView.java
+++ app/src/main/java/com/zxy/view_gradient/GradientView.java
@@ -19,44 +19,56 @@ public class GradientView extends View {
     public static final int START_COLOR = Color.parseColor("#274A8E");
     public static final int END_COLOR = Color.parseColor("#F3497C");
 
-    private float mStart = 0.0f;
-    private float mEnd = 1f;
+    private Paint mPaint;
+    private LinearGradient mGradient;
 
-    public GradientView(Context context) {
+    public GradientView(Context context)
+    {
         super(context);
         init();
     }
 
-    public GradientView(Context context, @Nullable AttributeSet attrs) {
+    public GradientView(Context context, @Nullable AttributeSet attrs)
+    {
         super(context, attrs);
         init();
     }
 
-    public void init() {
-
+    public void init()
+    {
+        mPaint = new Paint();
+        mGradient = new LinearGradient(0, 0, getMeasuredWidth(), 0,
+                new int[]{START_COLOR, END_COLOR},
+                new float[]{0.0f, 1.0f}, Shader.TileMode.CLAMP);
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas)
+    {
         super.onDraw(canvas);
-        Paint paint = new Paint();
-        LinearGradient gradient = new LinearGradient(0, 0, getMeasuredWidth(), 0,
-                new int[]{START_COLOR, END_COLOR},
-                new float[]{mStart, mEnd}, Shader.TileMode.CLAMP);
-        paint.setShader(gradient);
-        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), paint);
+        mPaint.setShader(mGradient);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
     }
 
-    public void setProgress(float left) {
+    public void setProgress(float left)
+    {
+        float start;
+        float end;
         //按照gradient分割线的位置进行平分
-        if (left <= 0.5f) {
-            mStart = 0.0f;
-            mEnd = left * 2;
-        } else {
-            mStart = left * 2 - 1;//left-(1-left)
-            mEnd = 1;
+        if (left <= 0.5f)
+        {
+            start = 0.0f;
+            end = left * 2;
+        } else
+        {
+            start = left * 2 - 1;//left-(1-left)
+            end = 1;
         }
+        mGradient = new LinearGradient(0, 0, getMeasuredWidth(), 0,
+                new int[]{START_COLOR, END_COLOR},
+                new float[]{start, end}, Shader.TileMode.CLAMP);
         invalidate();
     }
+
 
 }
