8c6a81db28583675d9ac308335a52d03a8ecb3d1
zxy <798486005@qq.com>
2019-03-26 17:10:46 +0800

    test get post(multipart/form-data)

--- app/src/test/java/com/zxy/demo/ExampleUnitTest.java
+++ app/src/test/java/com/zxy/demo/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.zxy.demo;
 
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 /**
@@ -20,8 +22,24 @@ public class ExampleUnitTest
     @Test
     public void test()
     {
-        HttpRequest request = HttpRequest.get("http://www.baidu.com");
-        String body = request.body();
-        System.out.println(body);
+        HttpRequest request = HttpRequest.get("https://www.baidu.com");
+        response_print(request);
+    }
+
+    private void response_print(HttpRequest request)
+    {
+        System.out.println(request.code());
+        System.out.println(request.body());
+    }
+
+    @Test
+    public void testMultipart_formData()
+    {
+        String url = "http://api.im.fanwe.cn/uploads?_token=U_e720375169e100e069a36dc2d4a08dc45c99dc8c8741f&scene=social";
+        HttpRequest request = HttpRequest.post(url);
+        File file = new File("D:\\Downloads\\首页.png");
+        System.out.println(file.getName() + " " + file.length());
+        request.part("files[]", file.getName(), file);
+        response_print(request);
     }
 }
