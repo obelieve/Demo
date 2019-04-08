99e3265b2ca1a0f450144407b35229427a4fd99a
zxy <798486005@qq.com>
2019-04-08 15:04:06 +0800

    singleInstance模式 A standard B singleInstance C standard 1.A->B->C->B B只会调用onNewIntent(); 2.A->B->C,然后一直回退，回退顺序是 C->A->B; C和A是同一个栈的回退完之后才是B;

---
+++ app/src/main/java/com/zxy/view_lifestyle/CActivity.java
@@ -0,0 +1,38 @@
+package com.zxy.view_lifestyle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+import com.zxy.utility.LogUtil;
+
+/**
+ * Created by admin on 2019/4/8.
+ */
+
+public class CActivity extends Activity {
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        LogUtil.e();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LogUtil.e();
+        Button button = new Button(this);
+        button.setText(getClass().getSimpleName()+" taskId:"+getTaskId());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CActivity.this,BActivity.class));
+            }
+        });
+        setContentView(button);
+    }
+}
