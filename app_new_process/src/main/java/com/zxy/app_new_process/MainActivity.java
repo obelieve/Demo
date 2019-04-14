e99515bc284e5073f2f31404c6033ba64ff06caf
zxy <798486005@qq.com>
2019-04-14 09:48:17 +0800

    创建app_new_process Module用于创建新的App进程

---
+++ app_new_process/src/main/java/com/zxy/app_new_process/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zxy.app_new_process;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
