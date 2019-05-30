5d03e39182498dcd2739de55fdea4e57c301f282
zxy <798486005@qq.com>
2019-05-30 23:10:55 +0800

    add HomeFragment Behavior (普通嵌套滚动)

--- app/src/main/java/com/zxy/demo/behavior/HomeContentBehavior.java
+++ app/src/main/java/com/zxy/demo/behavior/HomeContentBehavior.java
@@ -1,7 +1,29 @@
 package com.zxy.demo.behavior;
 
+import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HomeContentBehavior extends CoordinatorLayout.Behavior<RecyclerView> {
+
+    public HomeContentBehavior() {
+    }
+
+    public HomeContentBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, RecyclerView child, View dependency) {
+        return dependency instanceof LinearLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, RecyclerView child, View dependency) {
+        child.setY(dependency.getHeight()+dependency.getY());
+        return true;
+    }
 }
---
+++ app/src/main/java/com/zxy/demo/behavior/HomeTabBehavior.java
@@ -0,0 +1,58 @@
+package com.zxy.demo.behavior;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.NestedScrollingChild;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.zxy.utility.LogUtil;
+
+public class HomeTabBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
+
+    public HomeTabBehavior() {
+    }
+
+    public HomeTabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull LinearLayout child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull LinearLayout child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+        if (target instanceof RecyclerView) {
+            LogUtil.e("child:Y=" + child.getY());
+            int pos = ((LinearLayoutManager) ((RecyclerView) target).getLayoutManager()).findFirstCompletelyVisibleItemPosition();
+            if (dy < 0) {//向下滑
+                if (pos == 0 && child.getTranslationY() < 0) {
+                    float consumedY = dy;
+                    if (child.getTranslationY() - consumedY > 0) {
+                        consumedY = child.getTranslationY();
+                    }
+                    consumed[1] = (int) consumedY;
+                    child.setTranslationY(child.getTranslationY() - consumedY);
+                }
+            } else {//向上滑
+                if (pos == 0 && child.getTranslationY() > -child.getTop()) {
+                    float consumedY = dy;
+                    if (child.getTranslationY() - consumedY < -child.getTop()) {
+                        consumedY = child.getTop() + child.getTranslationY();
+                    }
+                    consumed[1] = (int) consumedY;
+                    child.setTranslationY(child.getTranslationY() - consumedY);
+                }
+            }
+        }
+    }
+}
