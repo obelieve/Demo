a169b54440e3dea1c7cc6f8c38d819a323a1c5f6
zxy <798486005@qq.com>
2019-06-02 09:57:00 +0800

    update progressOffset

--- app/src/main/java/com/zxy/demo/view/HomeSwipeRefreshLayout.java
+++ app/src/main/java/com/zxy/demo/view/HomeSwipeRefreshLayout.java
@@ -14,6 +14,6 @@ public class HomeSwipeRefreshLayout extends SwipeRefreshLayout {
     public HomeSwipeRefreshLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         float density = context.getResources().getDisplayMetrics().density;
-        setProgressViewOffset(false, (int)(density*8),(int)(density*16));//消除撤销刷新和tabLayout重叠阴影
+        setProgressViewOffset(false, 0,(int)(density*48));//消除撤销刷新和tabLayout重叠阴影
     }
 }
