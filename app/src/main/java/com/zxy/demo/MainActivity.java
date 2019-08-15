5ca9c780c3913aae0a126f3eb1bffdf5a30b6287
zxy <798486005@qq.com>
2019-08-15 23:12:32 +0800

    annotation

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.zxy.utility.LogUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,20 +31,31 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     *  主要四个操作符
+     *  requestEach requestEachCombined
+     *  ensureEach ensureEachCombined  这2个和RxView关联使用
+     *  其中Combined是多个权限全部授权才会granted，Each是按每个算的
+     * @param rxPermissions
+     */
     private void mode2(RxPermissions rxPermissions) {
         rxPermissions
                 .requestEachCombined(Manifest.permission.CAMERA,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .subscribe(permission -> { // will emit 2 Permission objects
+                .subscribe(permission -> { // will emit 1 Permission objects
                     if (permission.granted) {
                         // `permission.name` is granted !
-                        Toast.makeText(getBaseContext(),"已授权",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(),"已授权:"+permission.name,Toast.LENGTH_SHORT).show();
+                        LogUtil.e("已授权:"+permission.name);
                     } else if (permission.shouldShowRequestPermissionRationale) {
                         // Denied permission without ask never again
-                        Toast.makeText(getBaseContext(),"拒绝",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(),"未经允许不再询问"+permission.name,Toast.LENGTH_SHORT).show();
+                        LogUtil.e("未经允许不再询问:"+permission.name);
                     } else {
-                        // Denied permission with ask never again
-                        // Need to go to the settings
+                        // Denied permission with ask never again //永不询问
+                        // Need to go to the settings]
+
+                        LogUtil.e("永不询问:"+permission.name);
                         Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
                         Uri uri = Uri.fromParts("package", getPackageName(), null);
 
