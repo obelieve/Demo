7c7c239f512c9cdf59a4fb402c4c80144f6c33d8
zxy <798486005@qq.com>
2019-08-22 19:10:34 +0800

    update (如果Socket进行重新包装输出流在进行写入不可用)

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -11,57 +11,97 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.zxy.utility.LogUtil;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String SERVER_ACCEPT_ACTION = "SERVER_ACCEPT_ACTION";
     public static final String SERVER_ACCEPT_ACTION_EXTRA_STRING = "SERVER_ACCEPT_ACTION_EXTRA_STRING";
 
     private BluetoothAdapter mBluetoothAdapter;
     private BtReceiver mBtReceiver = new BtReceiver();
-    private TextView mTv;
-    private Button mBtn;
+    private TextView mTvLog;
+    private ListView mLvContent;
 
     private boolean isStart;
+    private Button mBtnScan;
+    private Button mBtnWrite;
     private Button mBtnServer;
-    private Button mBtnClient;
-    private BluetoothDevice mBluetoothDeviceConnect;
+    private BluetoothConnection mBluetoothConnection;
+    private String mConnect;
+    private int mInt;
+    private ArrayAdapter<String> mAdapter;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_scan:
+                if (!isStart) {
+                    mBluetoothAdapter.startDiscovery();//发现设备，异步进程，通过广播接收数据。
+                    mTvLog.append("发现设备信息：\n");
+                    isStart = true;
+                    mBtnScan.setText("停止");
+                } else {
+                    mBtnScan.setText("扫描");
+                    isStart = false;
+                    mBluetoothAdapter.cancelDiscovery();//在找到需要连接设备的MAC地址后，可关闭发现设备（发现设备操作会消耗大量资源）
+                    mTvLog.append("关闭发现设备。\n");
+                }
+                break;
+            case R.id.btn_server:
+                mConnect = "Server";
+                mBluetoothConnection.closeClient();
+                mBluetoothConnection.startServer();
+                break;
+            case R.id.btn_write:
+                if (mConnect != null) {
+                    if ("Server".equals(mConnect)) {
+                        mInt++;
+                        mBluetoothConnection.writeServer("客户端写到 随机Int:" + mInt);
+                    } else if ("Client".equals(mConnect)) {
+                        mInt++;
+                        mBluetoothConnection.writeClient("写到服务端随机Int:" + mInt);
+                    }
+                }
+                break;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBtn = findViewById(R.id.btn);
-        mTv = findViewById(R.id.tv);
-        mTv.setMovementMethod(ScrollingMovementMethod.getInstance());
-        mBtn.setOnClickListener(new View.OnClickListener() {
+        mTvLog = findViewById(R.id.tv_log);
+        mBtnScan = findViewById(R.id.btn_scan);
+        mBtnWrite = findViewById(R.id.btn_write);
+        mBtnServer = findViewById(R.id.btn_server);
+        mLvContent = findViewById(R.id.lv_content);
+        mAdapter = new ArrayAdapter<String>(this, R.layout.message);
+        mLvContent.setAdapter(mAdapter);
+        mBtnScan.setOnClickListener(this);
+        mBtnWrite.setOnClickListener(this);
+        mBtnServer.setOnClickListener(this);
+        mLvContent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                if (!isStart) {
-                    mBluetoothAdapter.startDiscovery();//发现设备，异步进程，通过广播接收数据。
-                    mTv.append("发现设备信息：\n");
-                    isStart = true;
-                    mBtn.setText("停止");
-                } else {
-                    mBtn.setText("扫描");
-                    isStart = false;
-                    mBluetoothAdapter.cancelDiscovery();//在找到需要连接设备的MAC地址后，可关闭发现设备（发现设备操作会消耗大量资源）
-                    mTv.append("关闭发现设备。\n");
-                }
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mConnect = "Client";
+                mBluetoothConnection.closeServer();
+                String info = ((TextView) view).getText().toString();
+                String address = info.substring(info.length() - 17);
+                mBluetoothConnection.startClient(mBluetoothAdapter.getRemoteDevice(address));
             }
         });
         ToastUtil.init(getApplicationContext());
@@ -78,23 +118,7 @@ public class MainActivity extends AppCompatActivity {
                 openBluetooth();
             }
         }
-
-        mBtnServer = findViewById(R.id.btn_server);
-        mBtnClient = findViewById(R.id.btn_client);
-        final BluetoothConnection bluetoothConnection = new BluetoothConnection(this, mBluetoothAdapter);
-        mBtnServer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                bluetoothConnection.startServer();
-            }
-        });
-        mBtnClient.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mBluetoothDeviceConnect != null)
-                    bluetoothConnection.startClient(mBluetoothDeviceConnect, mTv.getText().toString());
-            }
-        });
+        mBluetoothConnection = new BluetoothConnection(this, mBluetoothAdapter);
     }
 
     /**
@@ -103,12 +127,9 @@ public class MainActivity extends AppCompatActivity {
     private void showPairedDeviceInfo() {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         if (pairedDevices != null && pairedDevices.size() > 0) {
-            mTv.setText("已配对设备信息：\n");
+            mTvLog.setText("已配对设备信息：\n");
             for (BluetoothDevice device : pairedDevices) {
-                if("QCOM-BTD".equals(device.getName())){
-                    mBluetoothDeviceConnect = device;
-                }
-                mTv.append("device:" + device.getName() + "  " + device.getAddress() + "\n");
+                mAdapter.add("device:" + device.getName() + "  " + device.getAddress());
             }
         }
     }
@@ -138,7 +159,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void logger(String s) {
         LogUtil.e(s);
-        ToastUtil.show(s);
+
+        final String msgFinal = s;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTvLog.append(msgFinal + "\n");
+                ToastUtil.show(msgFinal);
+            }
+        });
     }
 
     @Override
@@ -156,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 蓝牙连接
      */
-    public static class BluetoothConnection {
+    public class BluetoothConnection {
 
         public static final String UUID = "00001101-0000-1000-8000-00805F9B34FB";
 
@@ -164,8 +193,8 @@ public class MainActivity extends AppCompatActivity {
         private ExecutorService mExecutorClient;
         private BluetoothAdapter mBluetoothAdapter;
         private BluetoothServerSocket mBluetoothServerSocket = null;
-        private BluetoothSocket mBluetoothSocketServer = null;
-        private BluetoothSocket mBluetoothSocketClient = null;
+        private BluetoothSocket mBluetoothSocketS = null;
+        private BluetoothSocket mBluetoothSocketC = null;
         private Context mContext;
 
         public BluetoothConnection(Context context, BluetoothAdapter adapter) {
@@ -176,83 +205,101 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void startServer() {
+            logger("蓝牙服务器监听：开始设置");
             mExecutorServer.execute(new Runnable() {
                 @Override
                 public void run() {
-
                     try {
-                        mBluetoothServerSocket = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("蓝牙", java.util.UUID.fromString(UUID));
+                        try {
+                            mBluetoothServerSocket = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("blue", java.util.UUID.fromString(UUID));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            logger("蓝牙服务器监听：初始化异常");
+                        }
+                        mBluetoothSocketS = mBluetoothServerSocket.accept();
+                        logger("蓝牙服务器监听：接收到请求");
                     } catch (IOException e) {
                         e.printStackTrace();
-                        LogUtil.e("服务ServerSocket初始化异常");
+                        logger("蓝牙服务器监听：请求错误");
                     }
-                    LogUtil.e("服务ServerSocket开始接收");
-                    while (true) {
-                        try {
-                            mBluetoothSocketServer = mBluetoothServerSocket.accept();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            break;
-                        }
-                        try {
-                            mBluetoothServerSocket.close();
-                            LogUtil.e("服务ServerSocket读取数据");
-                            readServer(mBluetoothSocketServer);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            break;
-                        }
+                    logger("蓝牙服务器监听：开始读取数据");
+                    readServer();
+                    try {
+                        mBluetoothServerSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
             });
         }
 
-        /**
-         * @param socket
-         */
-        private void readServer(BluetoothSocket socket) {
-            //服务端接收
-            LogUtil.e("readLine:"+socket);
-            try {
 
-                InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
-                BufferedReader reader = new BufferedReader(streamReader);
-                String s = null;
+        /**
+         *
+         */
+        public void readServer() {
+            //服务端接收
+            try {
+                byte[] buffer = new byte[1024];
                 StringBuilder stringBuilder = new StringBuilder();
-                LogUtil.e("readLine:"+reader.readLine());
-                while ((s = reader.readLine()) != null) {
-                    stringBuilder.append(s + "\n");
+                while (mBluetoothSocketS != null) {
+                    int b = mBluetoothSocketS.getInputStream().read(buffer);
+                    stringBuilder.append(new String(buffer, 0, b) + "\n");
+                    logger("蓝牙服务器监听：读取数据-完成-发送广播");
+                    Intent intent = new Intent(SERVER_ACCEPT_ACTION);
+                    intent.putExtra(SERVER_ACCEPT_ACTION_EXTRA_STRING, stringBuilder.toString());
+                    mContext.sendBroadcast(intent);//发送接收的数据
                 }
-                Intent intent = new Intent(SERVER_ACCEPT_ACTION);
-                intent.putExtra(SERVER_ACCEPT_ACTION_EXTRA_STRING,stringBuilder.toString());
-                mContext.sendBroadcast(intent);//发送接收的数据
             } catch (IOException e) {
                 e.printStackTrace();
+                logger("蓝牙服务器监听：读取数据-异常");
+            }
+        }
+
+        public void writeServer(String str) {
+            if (mBluetoothSocketS != null) {
+                try {
+                    mBluetoothSocketS.getOutputStream().write(str.getBytes());
+                    logger("写入到服务端信息："+str);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
 
-        public void startClient(final BluetoothDevice device, final String data) {
+        public void closeServer() {
+            if (mBluetoothSocketS != null) {
+                try {
+                    mBluetoothSocketS.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mBluetoothSocketS = null;
+            }
+        }
+
+
+        public void startClient(final BluetoothDevice device) {
             mExecutorClient.execute(new Runnable() {
                 @Override
                 public void run() {
-                    if (mBluetoothSocketClient != null) {
-                        try {
-                            mBluetoothSocketClient.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
                     try {
-                        mBluetoothSocketClient = device.createRfcommSocketToServiceRecord(java.util.UUID.fromString(UUID));
-                        mBluetoothAdapter.cancelDiscovery();
-                        mBluetoothSocketClient.connect();
-                        writeClient(mBluetoothSocketClient, data);
+                        mBluetoothSocketC = device.createRfcommSocketToServiceRecord(java.util.UUID.fromString(UUID));
+                        if (mBluetoothSocketC != null) {
+                            mBluetoothAdapter.cancelDiscovery();
+                            mBluetoothSocketC.connect();
+                            logger("蓝牙客户端主动连接：成功");
+                            Intent intent = new Intent(SERVER_ACCEPT_ACTION);
+                            intent.putExtra(SERVER_ACCEPT_ACTION_EXTRA_STRING, "蓝牙客户端主动连接：成功");
+                            mContext.sendBroadcast(intent);//发送接收的数据
+                            readClient();
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
-                        if (mBluetoothSocketClient != null) {
+                        if (mBluetoothSocketC != null) {
                             try {
-                                mBluetoothSocketClient.close();
+                                mBluetoothSocketC.close();
                             } catch (IOException e1) {
                                 e1.printStackTrace();
                             }
@@ -262,30 +309,44 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-        private void writeClient(BluetoothSocket socket, String data) {
+        public void writeClient(String data) {
             try {
-                OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
-                LogUtil.e("客户端写入："+data);
-                writer.write(data);
+                if (mBluetoothSocketC != null) {
+                    mBluetoothSocketC.getOutputStream().write(data.getBytes());
+                    logger("客户端写入：" + data);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        public void stopClient() {
-            if (mBluetoothSocketClient != null) {
-                try {
-                    mBluetoothSocketClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        /**
+         *
+         */
+        public void readClient() {
+            //客户端接收
+            try {
+                logger("蓝牙客户端监听：读取数据开始");
+                byte[] buffer = new byte[1024];
+                StringBuilder stringBuilder = new StringBuilder();
+                while (mBluetoothSocketC != null) {
+                    int b = mBluetoothSocketC.getInputStream().read(buffer);
+                    stringBuilder.append(new String(buffer, 0, b) + "\n");
+                    logger("蓝牙客户端监听：读取数据-完成-发送广播");
+                    Intent intent = new Intent(SERVER_ACCEPT_ACTION);
+                    intent.putExtra(SERVER_ACCEPT_ACTION_EXTRA_STRING, stringBuilder.toString());
+                    mContext.sendBroadcast(intent);//发送接收的数据
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger("蓝牙客户端监听：读取数据-异常");
             }
         }
 
-        public void stopServer() {
-            if (mBluetoothSocketServer != null) {
+        public void closeClient() {
+            if (mBluetoothSocketC != null) {
                 try {
-                    mBluetoothSocketServer.close();
+                    mBluetoothSocketC.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -304,7 +365,7 @@ public class MainActivity extends AppCompatActivity {
             IntentFilter intentFilter = new IntentFilter();
             intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);//蓝牙连接状态
             intentFilter.addAction(BluetoothDevice.ACTION_FOUND);//发现设备信息
-            intentFilter.addAction(SERVER_ACCEPT_ACTION);
+            intentFilter.addAction(SERVER_ACCEPT_ACTION);//获取蓝牙通信信息
             return intentFilter;
         }
 
@@ -315,17 +376,11 @@ public class MainActivity extends AppCompatActivity {
             } else if (intent.getAction().equals(BluetoothDevice.ACTION_FOUND)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 BluetoothClass bluetoothClass = intent.getParcelableExtra(BluetoothDevice.EXTRA_CLASS);
-                if ("QCOM-BTD".equals(device.getName())) {
-                    mBluetoothDeviceConnect = device;
-                    mTv.append("【存在】发现device:" + device.getName() + " addr:" + device.getAddress() + "\n");
-                } else {
-                    mTv.append("发现device:" + device.getName() + " addr:" + device.getAddress() + "\n");
-                }
-
+                mAdapter.add("device:" + device.getName() + "  " + device.getAddress());
             } else if (intent.getAction().equals(SERVER_ACCEPT_ACTION)) {
                 String s = intent.getStringExtra(SERVER_ACCEPT_ACTION_EXTRA_STRING);
-                mTv.append("接收数据：========================:\n");
-                mTv.append(s);
+                mTvLog.append("接收数据：========================:\n");
+                mTvLog.append(s);
             }
 
         }
