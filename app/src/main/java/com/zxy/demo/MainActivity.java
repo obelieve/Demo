c9c58085c735c5008f129ef25fb95520b1fa88a4
zxy <798486005@qq.com>
2019-11-10 11:37:19 +0800

    viewModel

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -3,37 +3,26 @@ package com.zxy.demo;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
-import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
-import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.zxy.demo.model.SquareListModel;
+import com.zxy.demo.viewmodel.MainViewModel;
 import com.zxy.frame.adapter.BaseQuickRecyclerViewAdapter;
 import com.zxy.frame.adapter.func.Func;
 import com.zxy.frame.adapter.item_decoration.VerticalItemDivider;
-import com.zxy.frame.json.MGson;
-import com.zxy.frame.utils.ToastUtil;
-
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,76 +34,53 @@ public class MainActivity extends AppCompatActivity {
 
     MainAdapter mMainAdapter;
 
-    int mCurPage = 1;
-    boolean mHasNextPage = false;
-
-    List<SquareListModel.DataBean.PostListBean> mPostListBeans = new ArrayList<>();
+    MainViewModel mMainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mMainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         mMainAdapter = new MainAdapter(this);
         mMainAdapter.setEmptyView(LayoutInflater.from(this).inflate(R.layout.view_empty, null));
         mMainAdapter.setOnLoadMoreListener(new Func.OnLoadMoreListener() {
             @Override
             public void onLoadMore() {
-                App.getServiceInterface().square_post(mCurPage).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        try {
-                            String json = response.body().string();
-                            SquareListModel model = MGson.newGson().fromJson(json, SquareListModel.class);
-                            mCurPage = model.getData().getCurrent_page();
-                            mMainAdapter.addData(model.getData().getPost_list());
-                            if (model.getData().getHas_next_page() == 1) {
-                                mCurPage++;
-                                mMainAdapter.loadMoreComplete();
-                            } else {
-                                mMainAdapter.loadMoreEnd();
-                            }
-//                            mMainAdapter.getDataHolder().addAll(model.getData().getPost_list())
-//                                    .notifyDataSetChanged();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-
-                    }
-                });
+                mMainViewModel.square_post(true);
             }
-        },mRvContent);
+        }, mRvContent);
         mRvContent.setLayoutManager(new LinearLayoutManager(this));
         mRvContent.addItemDecoration(new VerticalItemDivider());
         mRvContent.setAdapter(mMainAdapter);
         mSrlContent.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                mCurPage = 1;
-                App.getServiceInterface().square_post(mCurPage).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        try {
-                            String json = response.body().string();
-                            SquareListModel model = MGson.newGson().fromJson(json, SquareListModel.class);
-//                            mMainAdapter.getDataHolder().addAll(model.getData().getPost_list())
-//                                    .notifyDataSetChanged();
-                            mMainAdapter.setNewData(model.getData().getPost_list());
-                            mSrlContent.setRefreshing(false);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-
-                    }
-                });
+                mMainViewModel.square_post(false);
+            }
+        });
+        mMainViewModel.getListMutableLiveData().observe(this, new Observer<List<SquareListModel.DataBean.PostListBean>>() {
+            @Override
+            public void onChanged(List<SquareListModel.DataBean.PostListBean> postListBeans) {
+                mMainAdapter.setNewData(postListBeans);
+            }
+        });
+        mMainViewModel.getRefreshLiveData().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                mSrlContent.setRefreshing(aBoolean);
+            }
+        });
+        mMainViewModel.getLoadMoreLiveData().observe(this, new Observer<MainViewModel.LoadMoreModel>() {
+            @Override
+            public void onChanged(MainViewModel.LoadMoreModel loadMoreModel) {
+                if(loadMoreModel.isCompleted()){
+                    mMainAdapter.loadMoreComplete();
+                }else if(loadMoreModel.isEnd()){
+                    mMainAdapter.loadMoreEnd();
+                }else{
+                    mMainAdapter.loadMoreFail();
+                }
             }
         });
     }
---
+++ app/src/main/java/com/zxy/demo/viewmodel/MainViewModel.java
@@ -0,0 +1,119 @@
+package com.zxy.demo.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.zxy.demo.App;
+import com.zxy.demo.model.SquareListModel;
+import com.zxy.frame.json.MGson;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainViewModel extends ViewModel {
+
+    private MutableLiveData<List<SquareListModel.DataBean.PostListBean>> mListMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<Boolean> mRefreshLiveData = new MutableLiveData<>();
+    private MutableLiveData<LoadMoreModel> mLoadMoreLiveData = new MutableLiveData<>();
+
+    private int mCurPage;
+
+    public void square_post(boolean isMore) {
+        if (!isMore) {
+            mCurPage = 1;
+        }else{
+            mCurPage++;
+        }
+        App.getServiceInterface().square_post(mCurPage).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    String json = response.body().string();
+                    SquareListModel model = MGson.newGson().fromJson(json, SquareListModel.class);
+                    List<SquareListModel.DataBean.PostListBean> beans = model.getData().getPost_list();
+                    if (!isMore) {
+                        mListMutableLiveData.setValue(model.getData().getPost_list());
+                    } else {
+                        List<SquareListModel.DataBean.PostListBean> oldBeans = mListMutableLiveData.getValue();
+                        if (oldBeans != null) {
+                            oldBeans.addAll(beans);
+                            beans = oldBeans;
+                        }
+                        mListMutableLiveData.setValue(beans);
+                    }
+                    if (!isMore) {
+                        if (model.getData().getHas_next_page() == 0) {
+                            mLoadMoreLiveData.setValue(new LoadMoreModel(false, false, true));
+                        }
+                    }else{
+                        if (model.getData().getHas_next_page() == 0) {
+                            mLoadMoreLiveData.setValue(new LoadMoreModel(false, false, true));
+                        }else{
+                            mLoadMoreLiveData.setValue(new LoadMoreModel(true, false, false));
+                        }
+                    }
+                    mRefreshLiveData.setValue(false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    if (!isMore) {
+                        mRefreshLiveData.setValue(false);
+                    }else{
+                        mLoadMoreLiveData.setValue(new LoadMoreModel(false, true, false));
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                if (isMore) {
+                    mLoadMoreLiveData.setValue(new LoadMoreModel(false, true, false));
+                }else{
+                    mRefreshLiveData.setValue(false);
+                }
+            }
+        });
+    }
+
+    public MutableLiveData<List<SquareListModel.DataBean.PostListBean>> getListMutableLiveData() {
+        return mListMutableLiveData;
+    }
+
+    public MutableLiveData<Boolean> getRefreshLiveData() {
+        return mRefreshLiveData;
+    }
+
+    public MutableLiveData<LoadMoreModel> getLoadMoreLiveData() {
+        return mLoadMoreLiveData;
+    }
+
+    public static class LoadMoreModel {
+
+        private boolean mCompleted;
+        private boolean mError;
+        private boolean mEnd;
+
+        public LoadMoreModel(boolean completed, boolean error, boolean end) {
+            mCompleted = completed;
+            mError = error;
+            mEnd = end;
+        }
+
+        public boolean isCompleted() {
+            return mCompleted;
+        }
+
+        public boolean isError() {
+            return mError;
+        }
+
+        public boolean isEnd() {
+            return mEnd;
+        }
+    }
+}
