1a6879cd5067a6bb5911d22d3ec445d0e80c582b
zxy <798486005@qq.com>
2020-01-13 14:50:33 +0800

    添加支付宝支付SDK

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -1,19 +1,42 @@
 package com.zxy.demo;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.alipay.sdk.app.PayTask;
+import com.zxy.demo.utils.AuthResult;
+import com.zxy.demo.utils.Constant;
+import com.zxy.demo.utils.OrderInfoUtil2_0;
+import com.zxy.demo.utils.PayResult;
+
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+/**
+ * https://docs.open.alipay.com/54/104509 支付宝集成Demo下载
+ */
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.tv)
     TextView mTv;
+
+    private static final int SDK_PAY_FLAG = 1;
+    private static final int SDK_AUTH_FLAG = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +45,126 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @SuppressWarnings("unused")
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SDK_PAY_FLAG: {//支付结果返回
+                    @SuppressWarnings("unchecked")
+                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                    /**
+                     * 对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为9000则代表支付成功
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                        showAlert(MainActivity.this, getString(R.string.pay_success) + payResult);
+                    } else {
+                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                        showAlert(MainActivity.this, getString(R.string.pay_failed) + payResult);
+                    }
+                    break;
+                }
+                case SDK_AUTH_FLAG: {
+                    @SuppressWarnings("unchecked")
+                    AuthResult authResult = new AuthResult((Map<String, String>) msg.obj, true);
+                    String resultStatus = authResult.getResultStatus();
+
+                    // 判断resultStatus 为“9000”且result_code
+                    // 为“200”则代表授权成功，具体状态码代表含义可参考授权接口文档
+                    if (TextUtils.equals(resultStatus, "9000") && TextUtils.equals(authResult.getResultCode(), "200")) {
+                        // 获取alipay_open_id，调支付时作为参数extern_token 的value
+                        // 传入，则支付账户为该授权账户
+                        showAlert(MainActivity.this, getString(R.string.auth_success) + authResult);
+                    } else {
+                        // 其他状态值则为授权失败
+                        showAlert(MainActivity.this, getString(R.string.auth_failed) + authResult);
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        };
+    };
+
     @OnClick(R.id.tv)
     public void onViewClicked() {
+        String orderInfo = getOrderInfo();//获取订单签名信息，这个是服务端传下来的
+        if(TextUtils.isEmpty(orderInfo)){
+            return;
+        }
+        final Runnable payRunnable = new Runnable() {
 
+            @Override
+            public void run() {
+                PayTask alipay = new PayTask(MainActivity.this);
+                Map<String, String> result = alipay.payV2(orderInfo, true);//true 展示圆形进度条
+                Log.i("msp", result.toString());
+
+                Message msg = new Message();
+                msg.what = SDK_PAY_FLAG;
+                msg.obj = result;
+                mHandler.sendMessage(msg);
+            }
+        };
+
+        // 必须异步调用
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
+    }
+
+
+    public String getOrderInfo(){
+        if (TextUtils.isEmpty(Constant.APPID) || (TextUtils.isEmpty(Constant.RSA2_PRIVATE) && TextUtils.isEmpty(Constant.RSA_PRIVATE))) {
+            showAlert(this, getString(R.string.error_missing_appid_rsa_private));
+            return "";
+        }
+
+        /*
+         * 这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
+         * 真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
+         * 防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
+         *
+         * orderInfo 的获取必须来自服务端；
+         */
+        boolean rsa2 = (Constant.RSA2_PRIVATE.length() > 0);
+        Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap(Constant.APPID, rsa2);
+        String orderParam = OrderInfoUtil2_0.buildOrderParam(params);
+
+        String privateKey = rsa2 ? Constant.RSA2_PRIVATE : Constant.RSA_PRIVATE;
+        String sign = OrderInfoUtil2_0.getSign(params, privateKey, rsa2);
+        final String orderInfo = orderParam + "&" + sign;
+        return orderInfo;
+    }
+
+    private static void showAlert(Context ctx, String info) {
+        showAlert(ctx, info, null);
+    }
+
+    private static void showAlert(Context ctx, String info, DialogInterface.OnDismissListener onDismiss) {
+        new AlertDialog.Builder(ctx)
+                .setMessage(info)
+                .setPositiveButton(R.string.confirm, null)
+                .setOnDismissListener(onDismiss)
+                .show();
+    }
+
+    private static void showToast(Context ctx, String msg) {
+        Toast.makeText(ctx, msg, Toast.LENGTH_LONG).show();
+    }
+
+    private static String bundleToString(Bundle bundle) {
+        if (bundle == null) {
+            return "null";
+        }
+        final StringBuilder sb = new StringBuilder();
+        for (String key: bundle.keySet()) {
+            sb.append(key).append("=>").append(bundle.get(key)).append("\n");
+        }
+        return sb.toString();
     }
 }
---
+++ app/src/main/java/com/zxy/demo/utils/Constant.java
@@ -0,0 +1,31 @@
+package com.zxy.demo.utils;
+
+public class Constant {
+    /**
+     * 用于支付宝支付业务的入参 app_id。
+     */
+    public static final String APPID = "";
+
+    /**
+     * 用于支付宝账户登录授权业务的入参 pid。
+     */
+    public static final String PID = "";
+
+    /**
+     * 用于支付宝账户登录授权业务的入参 target_id。
+     */
+    public static final String TARGET_ID = "";
+
+    /**
+     *  pkcs8 格式的商户私钥。
+     *
+     * 	如下私钥，RSA2_PRIVATE 或者 RSA_PRIVATE 只需要填入一个，如果两个都设置了，本 Demo 将优先
+     * 	使用 RSA2_PRIVATE。RSA2_PRIVATE 可以保证商户交易在更加安全的环境下进行，建议商户使用
+     * 	RSA2_PRIVATE。
+     *
+     * 	建议使用支付宝提供的公私钥生成工具生成和获取 RSA2_PRIVATE。
+     * 	工具地址：https://doc.open.alipay.com/docs/doc.htm?treeId=291&articleId=106097&docType=1
+     */
+    public static final String RSA2_PRIVATE = "";
+    public static final String RSA_PRIVATE = "";
+}
