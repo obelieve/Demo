656926886dbef08699645b013cc9591ff96f4dac
zxy <798486005@qq.com>
2020-02-18 13:43:47 +0800

    添加滑动验证码

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -9,9 +9,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.zxy.utility.SystemUtil;
-
-import java.util.Random;
+import com.luozm.captcha.Captcha;
+import com.luozm.captcha.DefaultCaptchaStrategy;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,29 +24,53 @@ public class MainActivity extends AppCompatActivity {
     EditText etIdentify;
     @BindView(R.id.iv_identify)
     ImageView ivIdentify;
+    @BindView(R.id.captcha)
+    Captcha captcha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        ivIdentify.setImageBitmap(IdentifyingCode.getInstance().createBitmap(this,80,40));
+        ivIdentify.setImageBitmap(IdentifyingCode.getInstance().createBitmap(this, 80, 40));
+        captcha.setBitmap("https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3203825941,2278025487&fm=26&gp=0.jpg");
+        captcha.setCaptchaStrategy(new DefaultCaptchaStrategy(this));
+        captcha.setSeekBarStyle(R.drawable.po_seekbar,R.drawable.thumb);
+        captcha.setCaptchaListener(new Captcha.CaptchaListener() {
+            @Override
+            public String onAccess(long time) {
+                Toast.makeText(MainActivity.this,"验证成功",Toast.LENGTH_SHORT).show();
+                return "验证通过,耗时"+time+"毫秒";
+            }
+
+            @Override
+            public String onFailed(int failedCount) {
+                Toast.makeText(MainActivity.this,"验证失败",Toast.LENGTH_SHORT).show();
+                return "验证失败,已失败"+failedCount+"次";
+            }
+            @Override
+            public String onMaxFailed() {
+                Toast.makeText(MainActivity.this,"验证超过次数，你的帐号被封锁",Toast.LENGTH_SHORT).show();
+                return "验证失败,帐号已封锁";
+            }
+
+        });
     }
 
-    @OnClick({R.id.tv,R.id.iv_identify})
+    @OnClick({R.id.tv, R.id.iv_identify})
     public void onViewClicked(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.tv:
                 String code = IdentifyingCode.getInstance().getCode();
                 String tips = "错误";
-                if(etIdentify.getText().toString().toLowerCase().equals(code.toLowerCase())){
-                    Toast.makeText(this,"正确",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(this,tips,Toast.LENGTH_SHORT).show();
+                if (etIdentify.getText().toString().toLowerCase().equals(code.toLowerCase())) {
+                    Toast.makeText(this, "正确", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, tips, Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.iv_identify:
-                ivIdentify.setImageBitmap(IdentifyingCode.getInstance().createBitmap(this,80,40));
+                ivIdentify.setImageBitmap(IdentifyingCode.getInstance().createBitmap(this, 80, 40));
                 break;
         }
     }
