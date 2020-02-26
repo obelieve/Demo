0901c247946edee333e926833992a41c899dccd8
zxy <798486005@qq.com>
2020-02-26 16:02:52 +0800

    设置LineChart图表

--- app/src/main/java/com/zxy/demo/SchemeRecordChart.java
+++ app/src/main/java/com/zxy/demo/SchemeRecordChart.java
@@ -1,11 +1,21 @@
 package com.zxy.demo;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.graphics.drawable.GradientDrawable;
-import android.os.Bundle;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
@@ -15,54 +25,141 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.zxy.utility.LogUtil;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+public class SchemeRecordChart extends LinearLayout {
 
-public class MainActivity extends AppCompatActivity {
+    private static final int[] COLORS = new int[]{
+            Color.parseColor("#FF4C44"),
+            Color.parseColor("#FFA200"),
+            Color.parseColor("#333333")};
 
-    @BindView(R.id.tv)
-    TextView mTv;
-    @BindView(R.id.chart_line)
-    LineChart chartLine;
+    private static final String[] NAMES = new String[]{"红", "水", "黑"};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        List<Float> floatList = new ArrayList<>();
-        float[] entryVal = new float[]{0, 1, 1.66f, 2};
-        for (int i = 0; i < 30; i++) {
-            floatList.add(entryVal[new Random().nextInt(4)]);
-        }
-        List<RecordEntry> entryList = convertRecordEntry(floatList,new int[]{Color.parseColor("#FF4C44"), Color.parseColor("#FFA200"), Color.parseColor("#333333")});
-        LogUtil.e(entryList + " 萨达所");
-        //设置图表
-        settingChart(chartLine);//配置图表
-        setDataChart(chartLine, entryList);//设置数据
-        chartLine.invalidate();
+    private int[] mTagCounts = new int[3];
+    private List<Float> mRecordList;
 
+    private LineChart mLineChart;
+
+    public SchemeRecordChart(@NonNull Context context) {
+        super(context);
+        init();
     }
 
-    private static List<RecordEntry> convertRecordEntry(List<Float> floatList,int[] colors)  {
+    public SchemeRecordChart(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        setOrientation(VERTICAL);
+        mLineChart = new LineChart(getContext());
+        mLineChart.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        settingChart(mLineChart);
+        addView(mLineChart);
+        addView(genSchemeTagView(getContext()));
+    }
+
+    public void setRecordList(List<Float> recordList) {
+        if (recordList == null) {
+            recordList = new ArrayList<>();
+        }
+        recordList = mRecordList;
+        List<RecordEntry> recordEntryList = convertRecordEntry(recordList);
+        setDataChart(mLineChart,recordEntryList);
+        mLineChart.invalidate();
+    }
+
+
+    private LinearLayout genSchemeTagView(Context context) {
+        LinearLayout root = new LinearLayout(context);
+        root.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout.LayoutParams emptyParams = new LinearLayout.LayoutParams(0, 1);
+        emptyParams.weight = 1;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.weight = 1;
+        View emptyView = new View(context);
+        emptyView.setLayoutParams(emptyParams);
+        root.addView(emptyView);
+        root.addView(genSchemeTagItemView(context, params, NAMES[0], COLORS[0], mTagCounts[0]));
+        root.addView(genSchemeTagItemView(context, params, NAMES[1], COLORS[1], mTagCounts[1]));
+        root.addView(genSchemeTagItemView(context, params, NAMES[2], COLORS[2], mTagCounts[2]));
+        return root;
+    }
+
+    private View genSchemeTagItemView(Context context, ViewGroup.LayoutParams params, String name, @ColorInt int color, int count) {
+        final int WIDTH = dp2px(24);
+        final int TEXT_SIZE = dp2px(12);
+        final int TAG_TEXT_COLOR = Color.WHITE;
+        final int TAG_PADDING_LEFT = dp2px(10);
+
+        LinearLayout layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.setLayoutParams(params);
+        TextView tvTitle = new TextView(context);
+        tvTitle.setLayoutParams(new LinearLayout.LayoutParams(WIDTH, WIDTH));
+        tvTitle.setGravity(Gravity.CENTER);
+        tvTitle.setTextSize(TEXT_SIZE);
+        tvTitle.setTextColor(TAG_TEXT_COLOR);
+        tvTitle.setPadding(TAG_PADDING_LEFT, 0, 0, 0);
+        tvTitle.setBackground(genCircleShapeDrawable(color, 12));
+        tvTitle.setText(name);
+
+        TextView tvCount = new TextView(context);
+        tvCount.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        tvCount.setTextSize(TEXT_SIZE);
+        tvCount.setTextColor(color);
+        tvCount.setText(String.valueOf(count));
+        layout.addView(tvTitle);
+        layout.addView(tvCount);
+        return layout;
+    }
+
+    private int dp2px(int size) {
+        float density = getContext().getResources().getDisplayMetrics().density;
+        return (int) (size * density);
+    }
+
+    private ShapeDrawable genCircleShapeDrawable(@ColorInt int color, int radius) {
+        radius = dp2px(radius);
+        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+        drawable.getPaint().setColor(color);
+        Rect rect = new Rect();
+        rect.top = 0;
+        rect.left = 0;
+        rect.bottom = radius * 2;
+        rect.right = radius * 2;
+        drawable.setBounds(rect);
+        return drawable;
+    }
+
+    /**
+     * 转为RecordEntry列表
+     *
+     * @param floatList
+     * @return
+     */
+    private static List<RecordEntry> convertRecordEntry(List<Float> floatList) {
         List<RecordEntry> entryList = new ArrayList<>();
         for (int i = 0; i < floatList.size(); i++) {
             float x = i;
             float y = floatList.get(i);
-            int color = getEntryColor(y,colors);
+            int color = getEntryColor(y, COLORS);
             RecordEntry entry = new RecordEntry(x, y, color);
             entryList.add(entry);
         }
         return entryList;
     }
 
+    /**
+     * 设置点的颜色
+     *
+     * @param val
+     * @param colors
+     * @return
+     */
     public static int getEntryColor(float val, int[] colors) {
         int index;
         if (val == -2) {
@@ -75,23 +172,6 @@ public class MainActivity extends AppCompatActivity {
             index = 0;
         }
         return colors[index];
-    }
-
-    /**
-     * 设置图表数据
-     *
-     * @param lineChart
-     * @param entryList
-     */
-    private static void setDataChart(LineChart lineChart, List<RecordEntry> entryList) {
-        LineDataSet lineDataSet = genLineDataSet(entryList);//生成LineDataSet
-        LineData lineData = new LineData(lineDataSet);
-        //设置一页最大显示个数为10，超出部分就滑动
-        final int COUNT = 10;//一页最大显示个数为10
-        float ratio = (float) entryList.size() / (float) COUNT;
-        //显示的时候是按照多大的比率缩放显示,1f表示不放大缩小
-        lineChart.zoom(ratio, 0f, 0, 0);
-        lineChart.setData(lineData);
     }
 
     /**
@@ -110,6 +190,23 @@ public class MainActivity extends AppCompatActivity {
 
         lineChart.setDrawGridBackground(false);//绘制网格背景
         lineChart.getLegend().setEnabled(true);//是否绘制说明（标记-线的颜色和描述）
+    }
+
+    /**
+     * 设置图表数据
+     *
+     * @param lineChart
+     * @param entryList
+     */
+    private static void setDataChart(LineChart lineChart, List<RecordEntry> entryList) {
+        LineDataSet lineDataSet = genLineDataSet(entryList);//生成LineDataSet
+        LineData lineData = new LineData(lineDataSet);
+        //设置一页最大显示个数为10，超出部分就滑动
+        final int COUNT = 10;//一页最大显示个数为10
+        float ratio = (float) entryList.size() / (float) COUNT;
+        //显示的时候是按照多大的比率缩放显示,1f表示不放大缩小
+        lineChart.zoom(ratio, 0f, 0, 0);
+        lineChart.setData(lineData);
     }
 
     /**
@@ -152,7 +249,7 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param legend
      */
-    public static void setLegend(Legend legend) {
+    private static void setLegend(Legend legend) {
         final int TEXT_COLOR = Color.parseColor("#999999");
         final String[] LABELS = new String[]{"红", "水", "黑"};//3个
         final int[] COLORS = new int[]{Color.parseColor("#FF4C44"), Color.parseColor("#FFA200"), Color.parseColor("#333333")};//3个
@@ -193,7 +290,12 @@ public class MainActivity extends AppCompatActivity {
         legend.setCustom(entries);
     }
 
-    public static void axisStyle(LineChart chartLine) {
+    /**
+     * LineChart图表轴线样式
+     *
+     * @param chartLine
+     */
+    private static void axisStyle(LineChart chartLine) {
         final int LINE_COLOR = Color.parseColor("#F0F0F0");//线颜色
         final float GRANULARITY = 1.0f;//Y轴间隔
         final float Y_AXIS_MINIMUM = -0.1f;//Y轴最小值
@@ -220,11 +322,6 @@ public class MainActivity extends AppCompatActivity {
         YAxis rightAxis = chartLine.getAxisRight();
         rightAxis.setEnabled(false);
         chartLine.invalidate();
-    }
-
-    @OnClick(R.id.tv)
-    public void onViewClicked() {
-
     }
 
     public static class RecordEntry extends Entry {
