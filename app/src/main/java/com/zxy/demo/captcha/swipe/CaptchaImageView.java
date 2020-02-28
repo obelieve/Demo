9a1af6f7afc92b30043502548d749fc8d5cb1280
zxy <798486005@qq.com>
2020-02-28 10:37:57 +0800

    添加：成功、失败动画

--- app/src/main/java/com/zxy/demo/captcha/swipe/CaptchaImageView.java
+++ app/src/main/java/com/zxy/demo/captcha/swipe/CaptchaImageView.java
@@ -1,5 +1,8 @@
 package com.zxy.demo.captcha.swipe;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -7,6 +10,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
@@ -15,19 +19,31 @@ import java.util.Random;
 
 public class CaptchaImageView extends AppCompatImageView {
 
-    public final int BLOCK_SIZE = 50;
-    public final int ACCURACY_VALUE = 10;
+    private final int BLOCK_SIZE = 50;
+    private final int DRAW_SUCCESS_AREA_SIZE = 100;
+    private final int ACCURACY_VALUE = 10;
 
     private int mBlockSize;
+    private int mDrawSuccessAreaSize;
     private Paint mShadowPaint;
     private Paint mBlockPaint;
+    private Paint mDrawSuccessPaint;
     private SwipeCaptchaHelper.EndCallback mEndCallback;
     //init
+    private int mWidth;
+    private int mHeight;
     private int mFailedCount;
     private Path mBlockShadowPath;
     private Bitmap mBlockBitmap;
     private BlockPosition mBlockShadowPosition;
     private BlockPosition mBlockPosition;
+    //动画绘制参数
+    private boolean mDrawBlock = true;
+    private boolean mDrawSuccess = false;
+    private float mDrawSuccessOffset;
+    private ValueAnimator mFailureAnimator;
+    private ValueAnimator mSuccessAnimator;
+
     private SwipeCaptchaHelper.Status mStatus = SwipeCaptchaHelper.Status.INIT;
 
 
@@ -47,15 +63,22 @@ public class CaptchaImageView extends AppCompatImageView {
 
     private void init() {
         mBlockSize = (int) (getResources().getDisplayMetrics().density * BLOCK_SIZE);
+        mDrawSuccessAreaSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DRAW_SUCCESS_AREA_SIZE, getContext().getResources().getDisplayMetrics());
         mBlockPaint = new Paint();
         mShadowPaint = new Paint();
         mShadowPaint.setColor(Color.BLACK);
         mShadowPaint.setAlpha(165);
         mShadowPaint.setStyle(Paint.Style.FILL);
+        mDrawSuccessPaint = new Paint();
+        mDrawSuccessPaint.setColor(Color.WHITE);
+        mDrawSuccessPaint.setAlpha(165);
+        mDrawSuccessPaint.setStyle(Paint.Style.FILL);
     }
 
     private void initCaptcha() {
         mFailedCount = 0;
+        mWidth = getWidth();
+        mHeight = getHeight();
         int offset = mBlockSize;
         int border = ACCURACY_VALUE + 1;
         int x = new Random().nextInt(getWidth() - offset - border) + border;
@@ -69,6 +92,53 @@ public class CaptchaImageView extends AppCompatImageView {
         if (lastBlockBitmap != null) {//删除上一次滑块Bitmap
             lastBlockBitmap.recycle();
         }
+        mFailureAnimator = ValueAnimator.ofFloat(0, 1);
+        mFailureAnimator.setDuration(100).setRepeatCount(4);
+        mFailureAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        mFailureAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mDrawBlock = true;
+                captchaFailure();
+            }
+        });
+        mFailureAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                if (value < 0.5) {
+                    mDrawBlock = false;
+                } else {
+                    mDrawBlock = true;
+                }
+                invalidate();
+            }
+        });
+        mSuccessAnimator = ValueAnimator.ofFloat(mWidth + mDrawSuccessAreaSize, 0);
+        mSuccessAnimator.setDuration(500);
+        mSuccessAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                mDrawSuccess = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mDrawSuccess = false;
+                captchaSuccess();
+            }
+        });
+        mSuccessAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                mDrawSuccessOffset = value;
+                invalidate();
+            }
+        });
         mStatus = SwipeCaptchaHelper.Status.START;
     }
 
@@ -79,7 +149,14 @@ public class CaptchaImageView extends AppCompatImageView {
             initCaptcha();
         }
         canvas.drawPath(mBlockShadowPath, mShadowPaint);
-        canvas.drawBitmap(mBlockBitmap, mBlockPosition.x, mBlockPosition.y, mBlockPaint);
+        if (mDrawBlock) {
+            canvas.drawBitmap(mBlockBitmap, mBlockPosition.x, mBlockPosition.y, mBlockPaint);
+        }
+        if (mDrawSuccess) {
+            Path path = getDrawSuccessPath(mDrawSuccessAreaSize, mHeight);
+            path.offset(mDrawSuccessOffset, 0);
+            canvas.drawPath(path, mDrawSuccessPaint);
+        }
     }
 
     public void move(int progress) {
@@ -91,26 +168,35 @@ public class CaptchaImageView extends AppCompatImageView {
     }
 
     public void end() {
+        if (mStatus != SwipeCaptchaHelper.Status.MOVE) return;
         if (Math.abs((mBlockPosition.x - mBlockShadowPosition.x)) <= ACCURACY_VALUE && Math.abs(mBlockPosition.y - mBlockShadowPosition.y) <= ACCURACY_VALUE) {
             mBlockPosition.x = mBlockShadowPosition.x;
             mBlockPosition.y = mBlockShadowPosition.y;
             invalidate();
-            if (mEndCallback != null) {
-                mEndCallback.onSuccess();
-            }
+            mSuccessAnimator.start();
         } else {
             mFailedCount++;
-            if (mFailedCount >= SwipeCaptchaHelper.MAX_FAILED_COUNT) {
-                if (mEndCallback != null) {
-                    mEndCallback.onMaxFailed();
-                }
-            } else {
-                if (mEndCallback != null) {
-                    mEndCallback.onFailure();
-                }
-            }
+            mFailureAnimator.start();
         }
         mStatus = SwipeCaptchaHelper.Status.END;
+    }
+
+    private void captchaSuccess() {
+        if (mEndCallback != null) {
+            mEndCallback.onSuccess();
+        }
+    }
+
+    private void captchaFailure() {
+        if (mFailedCount >= SwipeCaptchaHelper.MAX_FAILED_COUNT) {
+            if (mEndCallback != null) {
+                mEndCallback.onMaxFailed();
+            }
+        } else {
+            if (mEndCallback != null) {
+                mEndCallback.onFailure();
+            }
+        }
     }
 
     public void resetBlock() {
@@ -158,6 +244,16 @@ public class CaptchaImageView extends AppCompatImageView {
         path.rLineTo(0, gap * 2);
         path.rLineTo(-mBlockSize, 0);
         path.rLineTo(0, -gap * 2);
+        path.close();
+        return path;
+    }
+
+    private Path getDrawSuccessPath(int size, int height) {
+        Path path = new Path();
+        path.moveTo(size, 0);
+        path.rLineTo(size / 2, height);
+        path.rLineTo(-size, 0);
+        path.rLineTo(-size / 2, -height);
         path.close();
         return path;
     }
