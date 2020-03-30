2d3aaa8043d25898ffba3fec33f69f1b0237f30d
zxy <798486005@qq.com>
2020-03-30 18:46:21 +0800

    statusBar

--- frame/src/main/java/com/zxy/frame/base/BaseActivity.java
+++ frame/src/main/java/com/zxy/frame/base/BaseActivity.java
@@ -1,50 +1,60 @@
 package com.zxy.frame.base;
 
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.zxy.frame.utils.ActivityUtil;
 import com.zxy.frame.utils.StatusBarUtil;
+
+import java.util.ArrayList;
 
 
 public class BaseActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 10240;
 
     private boolean mStatusBarLight = true;
     private int mStatusBarColor = Color.TRANSPARENT;
     protected boolean mNeedLightStatusBar = true;
 
+    /**
+     * Activity 实例.
+     */
+    protected AppCompatActivity mActivity;
+    private OnRequestPermissionListener mPermissionListener;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mActivity = this;
         setScreenOrientation();
-        if (mNeedLightStatusBar){
-            configStatusBar(defStatusBarLight(), defStatusBarColor());
+        if (mNeedLightStatusBar) {
+            configStatusBar(isStatusBarLight(), getStatusBarColor());
         }
     }
 
     protected void setScreenOrientation() {
         if (android.os.Build.VERSION.SDK_INT != Build.VERSION_CODES.O) {
-            try{
+            try {
                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//强制竖屏
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-
-    public boolean defStatusBarLight() {
-        return mStatusBarLight;
-    }
-
-    public int defStatusBarColor() {
-        return Color.TRANSPARENT;
-    }
-
 
     public int getStatusBarColor() {
         return mStatusBarColor;
@@ -52,10 +62,6 @@ public class BaseActivity extends AppCompatActivity {
 
     public boolean isStatusBarLight() {
         return mStatusBarLight;
-    }
-
-    public void configStatusBar(boolean light) {
-        configStatusBar(light, Color.TRANSPARENT);
     }
 
     public void configStatusBar(boolean light, @ColorInt int color) {
@@ -69,5 +75,109 @@ public class BaseActivity extends AppCompatActivity {
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             StatusBarUtil.setStatusBarTranslucentStatus(this);
         }
+    }
+
+
+    /**
+     * 请求权限
+     *
+     * @param permissions
+     * @param listener
+     */
+    public void requestPermission(final String[] permissions, OnRequestPermissionListener listener) {
+        this.mPermissionListener = listener;
+        //收集未授权或者拒绝过的权限
+        ArrayList<String> deniedPermissionList = new ArrayList<>();
+        for (String per : permissions) {
+            int checkSelfPermission = ContextCompat.checkSelfPermission(this, per);
+            if (checkSelfPermission == PackageManager.PERMISSION_DENIED) {
+                deniedPermissionList.add(per);
+            }
+        }
+
+        if (deniedPermissionList.isEmpty()) {
+            if (mPermissionListener != null) {
+                mPermissionListener.onSuccess();
+            }
+        } else {
+            String[] permissionArray = deniedPermissionList.toArray(new String[deniedPermissionList.size()]);
+            ActivityCompat.requestPermissions(this, permissionArray, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (PERMISSION_REQUEST_CODE == requestCode) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    if (shouldShowRequestPermissionRationale(permissions[i])) {
+                        showTipsDialog(permissions);
+                    } else {
+                        showSettingDialog();
+                    }
+                    return;
+                }
+            }
+
+            if (mPermissionListener != null) {
+                mPermissionListener.onSuccess();
+            }
+        }
+    }
+
+    /**
+     * 显示提示对话框
+     */
+    private void showTipsDialog(final String[] permissions) {
+        new AlertDialog.Builder(this)
+                .setTitle("提示")
+                .setMessage("需要授权相关权限")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (mPermissionListener != null) {
+                            mPermissionListener.onFailure();
+                        }
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+                    }
+                }).show();
+    }
+
+    private void showSettingDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("提示信息")
+                .setMessage("当前应用缺少必要权限，该功能暂时无法使用。如若需要，请单击【确定】按钮前往设置中心进行权限授权。")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (mPermissionListener != null) {
+                            mPermissionListener.onFailure();
+                        }
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityUtil.openAppSettings(mActivity);
+
+                    }
+                }).show();
+    }
+
+    public interface OnRequestPermissionListener {
+
+        void onSuccess();
+
+        void onFailure();
     }
 }
--- frame/src/main/java/com/zxy/frame/utils/StatusBarUtil.java
+++ frame/src/main/java/com/zxy/frame/utils/StatusBarUtil.java
@@ -11,12 +11,6 @@ import androidx.annotation.RequiresApi;
 
 public class StatusBarUtil {
 
-    private static Context sContext;
-
-    public static void init(Context context) {
-        sContext = context;
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static void setStatusBarTranslucentStatus(Activity activity) {
         activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -26,34 +20,42 @@ public class StatusBarUtil {
     public static void setStatusBarColor(Activity activity, int color) {
         Window window = activity.getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+//        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         window.setStatusBarColor(color);
     }
 
+    /**
+     * 设置状态栏字色和图标
+     *
+     * @param activity
+     * @param light    状态栏是否是浅色  是：状态栏字色和图标为黑色，否：状态栏字色和图标为白色
+     */
     @RequiresApi(api = Build.VERSION_CODES.M)
-    public static void setWindowLightStatusBar(Activity activity, boolean bool) {
+    public static void setWindowLightStatusBar(Activity activity, boolean light) {
         int systemUi = activity.getWindow().getDecorView().getSystemUiVisibility();
-        if (bool) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(systemUi | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        } else {
+        if (light) {
             activity.getWindow().getDecorView().setSystemUiVisibility(systemUi & (~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR));
+        } else {
+            activity.getWindow().getDecorView().setSystemUiVisibility(systemUi | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
     }
 
     /**
      * @return
      */
-    public static int getStatusBarHeight() {
+    public static int getStatusBarHeight(Context context) {
         int result = 0;
-        int resId = sContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int resId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
         if (resId > 0) {
-            result = sContext.getResources().getDimensionPixelOffset(resId);
+            result = context.getResources().getDimensionPixelOffset(resId);
         }
         return result;
     }
 
-    public static void fitsSystemWindows(View view) {
-        view.setPadding(view.getPaddingLeft(), view.getPaddingTop() + getStatusBarHeight(), view.getPaddingRight(), view.getPaddingBottom());
+    public static void fitsSystemWindows(Context context, View view) {
+        view.setPadding(view.getPaddingLeft(), view.getPaddingTop() + getStatusBarHeight(context), view.getPaddingRight(), view.getPaddingBottom());
     }
 }
