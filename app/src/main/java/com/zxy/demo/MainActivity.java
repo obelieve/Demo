4b50f9710c154cd0f25cc7f707b43119dcf0ab4c
zxy <zhangxyg@qq.com>
2020-04-02 15:05:24 +0800

    'u'

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -1,10 +1,20 @@
 package com.zxy.demo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.zxy.utility.LogUtil;
+
+import java.util.concurrent.Executors;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +30,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        toPrint();
+    }
+
+    public void toPrint() {
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String adid = AdvertisingIdClient.getGoogleAdId(getApplicationContext());
+                    LogUtil.e("adid:  " + adid);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        String ANDROID_ID = Settings.System.getString(getContentResolver(), Settings.System.ANDROID_ID);
+        LogUtil.e("Android ID: " + ANDROID_ID);
+        LogUtil.e("manufacturer: " + android.os.Build.MANUFACTURER);
+        getIMEI(this);
+    }
+
+    public void getIMEI(Context context) {
+        String imei = "";
+        try {
+            TelephonyManager tm = (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    Activity#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for Activity#requestPermissions for more details.
+                    requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+                    return;
+                }
+                LogUtil.e("Imei: " + tm.getImei());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    LogUtil.e("Imei: " + tm.getImei());
+                }
+                return;
+            }
+        }
     }
 
     @OnClick(R.id.tv)
