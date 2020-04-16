a3ab9539c89073fa1f66e0f1a8f95fbb00f8ee32
zxy <zhangxyg@qq.com>
2020-04-16 15:43:29 +0800

    修改Lottie

--- app/src/main/java/com/zxy/demo/_issue/LottieAnimationFragment.java
+++ app/src/main/java/com/zxy/demo/_issue/LottieAnimationFragment.java
@@ -4,11 +4,12 @@ import com.airbnb.lottie.LottieAnimationView;
 import com.zxy.demo.R;
 import com.zxy.frame.base.BaseFragment;
 
-import java.util.Random;
-
 import butterknife.BindView;
 import butterknife.OnClick;
 
+/**
+ * ((DefaultItemAnimator) #RecyclerView.getItemAnimator()).setSupportsChangeAnimations(false);//RecyclerView item中使用，要调用 （动画有冲突）
+ */
 public class LottieAnimationFragment extends BaseFragment {
 
     @BindView(R.id.animation_view)
@@ -27,10 +28,11 @@ public class LottieAnimationFragment extends BaseFragment {
 
     @OnClick(R.id.animation_view)
     public void onViewClicked() {
-        int index = new Random().nextInt(mImageJson.length);
-        animationView.cancelAnimation();
-        animationView.setImageAssetsFolder(mImageJson[index]+"/images");
-        animationView.setAnimation(mImageJson[index]+"/"+mImageJson[index]+".json");
         animationView.playAnimation();
+//        int index = new Random().nextInt(mImageJson.length);
+//        animationView.cancelAnimation();
+//        animationView.setImageAssetsFolder(mImageJson[index]+"/images");
+//        animationView.setAnimation(mImageJson[index]+"/"+mImageJson[index]+".json");
+//        animationView.playAnimation();
     }
 }
