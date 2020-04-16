45a9322baf7e6d3c0f945e1927f5b9165a59602a
zxy <zhangxyg@qq.com>
2020-04-16 14:36:36 +0800

    添加Lottie动画效果，LottieAnimationFragment

---
+++ app/src/main/java/com/zxy/demo/_issue/LottieAnimationFragment.java
@@ -0,0 +1,36 @@
+package com.zxy.demo._issue;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.zxy.demo.R;
+import com.zxy.frame.base.BaseFragment;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class LottieAnimationFragment extends BaseFragment {
+
+    @BindView(R.id.animation_view)
+    LottieAnimationView animationView;
+    String[] mImageJson = new String[]{"community", "data", "find", "match", "me", "refresh", "zan"};
+
+    @Override
+    public int layoutId() {
+        return R.layout.fragment_lottie_animation;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @OnClick(R.id.animation_view)
+    public void onViewClicked() {
+        int index = new Random().nextInt(mImageJson.length);
+        animationView.cancelAnimation();
+        animationView.setImageAssetsFolder(mImageJson[index]+"/images");
+        animationView.setAnimation(mImageJson[index]+"/"+mImageJson[index]+".json");
+        animationView.playAnimation();
+    }
+}
