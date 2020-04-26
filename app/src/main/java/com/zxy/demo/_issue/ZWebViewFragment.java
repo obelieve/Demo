d6072a1b1a72a1781859322d759d5a7eacea944c
zxy <zhangxyg@qq.com>
2020-04-26 09:18:32 +0800

    添加AppWebView显示

--- app/src/main/java/com/zxy/demo/_issue/ZWebViewFragment.java
+++ app/src/main/java/com/zxy/demo/_issue/ZWebViewFragment.java
@@ -1,9 +1,19 @@
 package com.zxy.demo._issue;
 
+import android.webkit.JavascriptInterface;
+
 import com.zxy.demo.R;
 import com.zxy.frame.base.BaseFragment;
+import com.zxy.frame.utils.ToastUtil;
+import com.zxy.frame.view.AppWebView;
+
+import butterknife.BindView;
 
 public class ZWebViewFragment extends BaseFragment {
+
+    @BindView(R.id.wv_content)
+    AppWebView wvContent;
+
     @Override
     public int layoutId() {
         return R.layout.fragment_zwebview;
@@ -11,6 +21,17 @@ public class ZWebViewFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        wvContent.loadUrl("https://www.baidu.com");
+//        wvContent.addJavascriptInterface(new JSInvoke(),"Android");
+//        wvContent.setWebContentsDebuggingEnabled(true);
+//        wvContent.getSettings().setUserAgentString("Android");
+    }
 
+
+    public static class JSInvoke{
+        @JavascriptInterface
+        public void login(){
+            ToastUtil.show("登录");
+        }
     }
 }
