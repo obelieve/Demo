13787f2e5ce0c177233a667fd37be26b1ad5fe58
zxy <zhangxyg.@qq.com>
2020-06-01 22:51:31 +0800

    u RatingHeaderView

--- mall/src/main/java/com/zxy/mall/fragment/RatingFragment.java
+++ mall/src/main/java/com/zxy/mall/fragment/RatingFragment.java
@@ -2,8 +2,14 @@ package com.zxy.mall.fragment;
 
 import com.zxy.frame.base.BaseFragment;
 import com.zxy.mall.R;
+import com.zxy.mall.view.header.RatingHeaderView;
+
+import butterknife.BindView;
 
 public class RatingFragment extends BaseFragment {
+
+    @BindView(R.id.view_rating_header)
+    RatingHeaderView mViewRatingHeader;
 
     @Override
     public int layoutId() {
--- mall/src/main/java/com/zxy/mall/view/header/RatingHeaderView.java
+++ mall/src/main/java/com/zxy/mall/view/header/RatingHeaderView.java
@@ -4,16 +4,48 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.FrameLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.zxy.mall.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RatingHeaderView extends FrameLayout {
+
+    @BindView(R.id.tv_name)
+    TextView mTvName;
+    @BindView(R.id.tv_sum_rating)
+    TextView mTvSumRating;
+    @BindView(R.id.tv_info)
+    TextView mTvInfo;
+    @BindView(R.id.view)
+    View mView;
+    @BindView(R.id.tv_title1)
+    TextView mTvTitle1;
+    @BindView(R.id.rb_rating1)
+    RatingBar mRbRating1;
+    @BindView(R.id.tv_rating1)
+    TextView mTvRating1;
+    @BindView(R.id.tv_title2)
+    TextView mTvTitle2;
+    @BindView(R.id.rb_rating2)
+    RatingBar mRbRating2;
+    @BindView(R.id.tv_rating2)
+    TextView mTvRating2;
+    @BindView(R.id.tv_delivery_time)
+    TextView mTvDeliveryTime;
+    @BindView(R.id.rv_rating_category)
+    RecyclerView mRvRatingCategory;
+    @BindView(R.id.cb_select_content)
+    CheckBox mCbSelectContent;
 
     public RatingHeaderView(@NonNull Context context) {
         super(context);
