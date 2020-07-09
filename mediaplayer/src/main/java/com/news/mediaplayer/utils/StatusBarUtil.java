2489b4582f3d94e6692538c4be4c39f8ef285599
zxy <zhangxyg@qq.com>
2020-07-09 11:35:26 +0800

    u 屏幕全屏

--- mediaplayer/src/main/java/com/news/mediaplayer/utils/StatusBarUtil.java
+++ mediaplayer/src/main/java/com/news/mediaplayer/utils/StatusBarUtil.java
@@ -32,7 +32,15 @@ public class StatusBarUtil {
     }
 
     public static void setFullScreen(Activity activity) {
-        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+        lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        activity.getWindow().setAttributes(lp);
+        activity.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     /**
