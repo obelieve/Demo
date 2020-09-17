7d12fd077255de343d0ac6304cdd9d203bef5b78
zxy <zhangxyg@qq.com>
2020-09-17 16:42:02 +0800

    u

--- app/src/main/java/com/zxy/demo/AppActivity.java
+++ app/src/main/java/com/zxy/demo/AppActivity.java
@@ -25,7 +25,7 @@ public class AppActivity extends Activity {
 
     @OnClick(R.id.tv)
     public void onViewClicked() {
-        RePlugin.startActivity(AppActivity.this, RePlugin.createIntent("com.github.obelieve.plugin",
+        RePlugin.startActivity(AppActivity.this, RePlugin.createIntent("plugin",
                 "com.github.obelieve.plugin.SplashActivity"));
     }
 }
