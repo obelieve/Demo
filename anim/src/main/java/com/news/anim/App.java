4d2e0e76cf83dcee85a507014acb96366bb92c40
zxy <zhangxyg@qq.com>
2020-10-09 17:44:29 +0800

    update：[anim Module] App

--- anim/src/main/java/com/news/anim/App.java
+++ anim/src/main/java/com/news/anim/App.java
@@ -3,14 +3,12 @@ package com.news.anim;
 import android.app.Application;
 
 import com.zxy.frame.utils.ToastUtil;
-import com.zxy.utility.SystemUtil;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        SystemUtil.init(this);
         ToastUtil.init(this);
     }
 
