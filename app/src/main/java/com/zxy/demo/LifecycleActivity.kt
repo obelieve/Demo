598029d551dd9be2ba6085c2eca6be074de68fba
zxy <zhangxyg@qq.com>
2020-11-04 15:08:50 +0800

    update：MediatorLiveData使用

--- app/src/main/java/com/zxy/demo/LifecycleActivity.kt
+++ app/src/main/java/com/zxy/demo/LifecycleActivity.kt
@@ -1,18 +1,10 @@
 package com.zxy.demo;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Handler
-import android.util.Log
-
-import androidx.annotation.Nullable;
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.*
-import androidx.lifecycle.Observer
 import com.zxy.utility.LogUtil
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import javax.security.auth.login.LoginException
 
 /**
  * Created by Admin
@@ -20,48 +12,50 @@ import javax.security.auth.login.LoginException
  */
 class LifecycleActivity : AppCompatActivity() {
 
-    var registery: LifecycleRegistry? =null
-    var viewModel:LifecycleViewModel?=null
+    var registery: LifecycleRegistry? = null
+    var viewModel: LifecycleViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LogUtil.e()
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(LifecycleViewModel::class.java)
+        viewModel?.aMediatorLiveData?.observe(this, Observer {
+            LogUtil.e("MediatorLiveData监听A:$it")
+        })
         viewModel!!.value.observe(this, Observer {
-            tv.text="$it"
+            tv.text = "$it"
         })
         lifecycle.addObserver(TestObserver())
-        lifecycle.addObserver(object: DefaultLifecycleObserver{
+        lifecycle.addObserver(object : DefaultLifecycleObserver {
             override fun onCreate(owner: LifecycleOwner) {
-                LogUtil.e("DefaultLifecycleObserver-onCreate","onCreate")
+                LogUtil.e("DefaultLifecycleObserver-onCreate", "onCreate")
             }
 
             override fun onStart(owner: LifecycleOwner) {
-                LogUtil.e("DefaultLifecycleObserver-onStart","onStart")
+                LogUtil.e("DefaultLifecycleObserver-onStart", "onStart")
             }
 
             override fun onResume(owner: LifecycleOwner) {
-                LogUtil.e("DefaultLifecycleObserver-onResume","onResume")
+                LogUtil.e("DefaultLifecycleObserver-onResume", "onResume")
             }
 
             override fun onPause(owner: LifecycleOwner) {
-                LogUtil.e("DefaultLifecycleObserver-onPause","onPause")
+                LogUtil.e("DefaultLifecycleObserver-onPause", "onPause")
             }
 
             override fun onStop(owner: LifecycleOwner) {
-                LogUtil.e("DefaultLifecycleObserver-onStop","onStop")
+                LogUtil.e("DefaultLifecycleObserver-onStop", "onStop")
             }
 
             override fun onDestroy(owner: LifecycleOwner) {
-                LogUtil.e("DefaultLifecycleObserver-onDestroy","onDestroy")
+                LogUtil.e("DefaultLifecycleObserver-onDestroy", "onDestroy")
             }
         });
         registery = LifecycleRegistry(this);
         registery!!.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
         registery!!.addObserver(TestObserver());
     }
-
 
 
     override fun onStart() {
--- app/src/main/java/com/zxy/demo/LifecycleViewModel.java
+++ app/src/main/java/com/zxy/demo/LifecycleViewModel.java
@@ -1,6 +1,8 @@
 package com.zxy.demo;
 
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 /**
@@ -10,7 +12,27 @@ import androidx.lifecycle.ViewModel;
 public class LifecycleViewModel extends ViewModel {
 
     private MutableLiveData<A> mValueLiveData = new MutableLiveData<>();
+    private MutableLiveData<String> mNameLiveData = new MutableLiveData<>();
 
+    /**
+     * 监听LiveData
+     */
+    private MediatorLiveData<Object> mAMediatorLiveData = new MediatorLiveData();
+    {
+        mAMediatorLiveData.addSource(mNameLiveData, new Observer<String>() {
+            @Override
+            public void onChanged(String str) {
+                mAMediatorLiveData.setValue(str);
+            }
+        });
+        mAMediatorLiveData.addSource(mValueLiveData, new Observer<A>() {
+            @Override
+            public void onChanged(A a) {
+                mAMediatorLiveData.setValue(a);
+            }
+        });
+
+    }
     private int mCount = 0;
     private boolean mOpen = true;
 
@@ -25,6 +47,7 @@ public class LifecycleViewModel extends ViewModel {
                 while (mOpen){
                     a.setCount(mCount++);
                     mValueLiveData.postValue(a);
+                    mNameLiveData.postValue(mCount+"值");
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -45,6 +68,10 @@ public class LifecycleViewModel extends ViewModel {
 
     public MutableLiveData<A> getValue() {
         return mValueLiveData;
+    }
+
+    public MediatorLiveData<Object> getAMediatorLiveData() {
+        return mAMediatorLiveData;
     }
 
     public static class A{
