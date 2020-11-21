f0f2077f8705f57d0922e4cfc4fa5898187e94f5
zxy <zhangxyg.@qq.com>
2020-11-21 23:19:41 +0800

    update:[ui] add CircleProgressView

---
+++ ui/src/main/java/com/zxy/ui/ViewFragment.java
@@ -0,0 +1,12 @@
+package com.zxy.ui;
+
+import com.news.anim.databinding.FragmentViewBinding;
+import com.zxy.frame.base.ApiBaseFragment;
+
+public class ViewFragment extends ApiBaseFragment<FragmentViewBinding> {
+
+    @Override
+    protected void initView() {
+
+    }
+}
