2bb5f3ca19ea0ef41e02905d0cc3e996acf0c53b
zxy <zhangxyg@qq.com>
2020-12-29 10:41:12 +0800

    fix：引导页全屏显示

--- app/src/main/java/com/zxy/demo/activity/SplashActivity.java
+++ app/src/main/java/com/zxy/demo/activity/SplashActivity.java
@@ -2,14 +2,24 @@ package com.zxy.demo.activity;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+
 import com.zxy.demo.databinding.ActivitySplashBinding;
 import com.zxy.frame.base.ApiBaseActivity2;
+import com.zxy.frame.utils.StatusBarUtil;
 
 /**
  * Created by Admin
  * on 2020/6/23
  */
 public class SplashActivity extends ApiBaseActivity2<ActivitySplashBinding> {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StatusBarUtil.setHideStatusBarFullScreen(mActivity);
+    }
+
     @Override
     protected void initCreateAfterView(Bundle savedInstanceState) {
 
