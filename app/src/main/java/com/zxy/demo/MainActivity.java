9a2b1130984fa101f56722a9cdea8f027d88a70a
zxy <zhangxyg@qq.com>
2021-03-23 18:29:55 +0800

    Update  Add RecyclerView源码

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -2,9 +2,18 @@ package com.zxy.demo;
 
 import android.os.Bundle;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +29,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        RecyclerView rv = findViewById(R.id.rv_content);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new RecyclerView.Adapter() {
+            List<String> list = new ArrayList<>();
+
+            {
+                for(int i=0;i<10;i++){
+                    list.add("i="+i);
+                }
+            }
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new BaseViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_main,parent,false));
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                ((BaseViewHolder)holder).bind(list.get(position));
+            }
+
+            @Override
+            public int getItemCount() {
+                return list.size();
+            }
+
+            class BaseViewHolder extends RecyclerView.ViewHolder{
+
+                public BaseViewHolder(@NonNull View itemView) {
+                    super(itemView);
+
+                }
+
+                public void bind(String s){
+                    TextView tv = itemView.findViewById(R.id.tv);
+                    tv.setText(s);
+                }
+
+            }
+        });
     }
 
     @OnClick(R.id.tv)
