fe2765ed6e625d6dbf9ee3e85862c2096e3c898b
zxy <zhangxyg@qq.com>
2021-04-13 10:16:40 +0800

    Update HJ1_2_write

--- app/src/main/java/com/zxy/demo/hw/HJ1_2_write.java
+++ app/src/main/java/com/zxy/demo/hw/HJ1_2_write.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 
 /**
  * # 字符串	简单
- * @User: 吴广谋
- * @Date: 2020/6/24
  * @Description: 计算字符串最后一个单词的长度，单词以空格隔开。
  * 输入描述：一行字符串，非空，长度小于5000       输出描述：整数N，最后一个单词的长度
  * 例：输入："hello world"     输出：5
  */
-public class HJ1_2 {
+public class HJ1_2_write {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        String str = input.nextLine();
-        String[] splitArr = str.split(" ");
-        int length = splitArr.length;
-        System.out.println(splitArr[length-1].length());
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            int lastIndex = line.lastIndexOf(" ");
+            System.out.println(line.substring(lastIndex+1).length());
+        }
     }
 }
--- app/src/main/java/com/zxy/demo/hw/HJ2_2.java
+++ app/src/main/java/com/zxy/demo/hw/HJ2_2.java
@@ -3,8 +3,6 @@ package com.zxy.demo.hw;
 import java.util.Scanner;
 
 /**
- * @User: 吴广谋
- * @Date: 2020/6/29
  * @Description: 写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
  * 输入描述：第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
  * 输出描述：输出输入字符串中含有该字符的个数。
@@ -14,15 +12,8 @@ public class HJ2_2 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
 
-        String str = input.nextLine();
-        String target = input.next();
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (String.valueOf(str.charAt(i)).equalsIgnoreCase(target)){
-                count++;
-            }
         }
-        System.out.println(count);
     }
 }
