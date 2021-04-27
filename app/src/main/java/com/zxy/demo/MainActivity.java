cdca7c457ea7aa65ca401593005b3e0ffc926015
zxy <zhangxyg@qq.com>
2021-04-27 17:56:29 +0800

    Update

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -13,32 +13,33 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.zxy.utility.LogUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
 
 /**
- 1.RecyclerView怎么根据LayoutManager把，ViewHolder加载进去?
- 2.怎么复用View？
-
- Update RecyclerView布局流程：
- 从->onMeasure()/onLayout()
- ->dispatchLayout()
-  ｛
-    dispatchLayoutStep1(); //一些初始化操作，记录状态
-    dispatchLayoutStep2();
-    ｛
-        #mLayoutManager.onLayoutChildren(mRecycler, mState);
-        {
-            fill(recycler, mLayoutState, state, false);//*测量和布局
-            //有一个while轮询语句块
-        }
-     ｝
-    dispatchLayoutStep3();
- ｝
-
-**/
+ * 1.RecyclerView怎么根据LayoutManager把，ViewHolder加载进去?
+ * 2.怎么复用View？
+ * <p>
+ * Update RecyclerView布局流程：
+ * 从->onMeasure()/onLayout()
+ * ->dispatchLayout()
+ * ｛
+ * dispatchLayoutStep1(); //一些初始化操作，记录状态
+ * dispatchLayoutStep2();
+ * ｛
+ * #mLayoutManager.onLayoutChildren(mRecycler, mState);
+ * {
+ * fill(recycler, mLayoutState, state, false);//*测量和布局
+ * //有一个while轮询语句块
+ * }
+ * ｝
+ * dispatchLayoutStep3();
+ * ｝
+ **/
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -60,19 +61,19 @@ public class MainActivity extends AppCompatActivity {
             List<String> list = new ArrayList<>();
 
             {
-                for(int i=0;i<10;i++){
-                    list.add("i="+i);
+                for (int i = 0; i < 30; i++) {
+                    list.add("i=" + i);
                 }
             }
             @NonNull
             @Override
             public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return new BaseViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_main,parent,false));
+                return new BaseViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_main, parent, false));
             }
 
             @Override
             public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-                ((BaseViewHolder)holder).bind(list.get(position));
+                ((BaseViewHolder) holder).bind(list.get(position),position);
             }
 
             @Override
@@ -80,14 +81,15 @@ public class MainActivity extends AppCompatActivity {
                 return list.size();
             }
 
-            class BaseViewHolder extends RecyclerView.ViewHolder{
+            class BaseViewHolder extends RecyclerView.ViewHolder {
 
                 public BaseViewHolder(@NonNull View itemView) {
                     super(itemView);
-
+                    LogUtil.e("BaseViewHolder 创建 " + this);
                 }
 
-                public void bind(String s){
+                public void bind(String s, int pos) {
+                    LogUtil.e("BaseViewHolder pos = " + pos + " 绑定 " + this);
                     TextView tv = itemView.findViewById(R.id.tv);
                     tv.setText(s);
                 }
