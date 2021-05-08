3a2e78f827d9722fbe116c1936d5540382e19ffd
zxy <zhangxyg@qq.com>
2021-05-08 15:42:00 +0800

    Update show demo

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -1,10 +1,16 @@
 package com.zxy.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.hencoder.hencoderpracticedraw1.practice.MainActivity1;
+import com.hencoder.hencoderpracticedraw2.MainActivity2;
+import com.hencoder.hencoderpracticedraw3.MainActivity3;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,8 +28,18 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.tv)
-    public void onViewClicked() {
-
+    @OnClick({R.id.tv,R.id.tv1,R.id.tv2,R.id.tv3})
+    public void onViewClicked(View view) {
+        switch (view.getId()){
+            case R.id.tv1:
+                startActivity(new Intent(this, MainActivity1.class));
+                break;
+            case R.id.tv2:
+                startActivity(new Intent(this, MainActivity2.class));
+                break;
+            case R.id.tv3:
+                startActivity(new Intent(this, MainActivity3.class));
+                break;
+        }
     }
 }
