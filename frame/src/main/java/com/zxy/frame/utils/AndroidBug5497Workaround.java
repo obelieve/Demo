699b58a8c2cd715a92d796393868884f53c5e87f
zxy <zhangxyg@qq.com>
2021-05-13 15:17:05 +0800

    Update AndroidBug5497Workaround 页面中存在，横竖屏切换问题处理(PS:直播全屏播放)

--- frame/src/main/java/com/zxy/frame/utils/AndroidBug5497Workaround.java
+++ frame/src/main/java/com/zxy/frame/utils/AndroidBug5497Workaround.java
@@ -1,77 +1,46 @@
 package com.zxy.frame.utils;
 
-import android.app.Activity;
 import android.graphics.Rect;
-import android.os.Build;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.FrameLayout;
 
 public class AndroidBug5497Workaround {
-    public static void assistActivity(Activity activity) {
-        new AndroidBug5497Workaround(activity);
+    public static void assistActivity(View content) {
+        new AndroidBug5497Workaround(content);
     }
+
     private View mChildOfContent;
     private int usableHeightPrevious;
-    private FrameLayout.LayoutParams frameLayoutParams;
-    private int contentHeight;
-    private   boolean isfirst = true;
-    private   Activity activity;
-    private  int statusBarHeight;
+    private ViewGroup.LayoutParams frameLayoutParams;
 
-    private AndroidBug5497Workaround(Activity activity) {
-        //获取状态栏的高度
-        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
-        this.activity = activity;
-        FrameLayout content = (FrameLayout)activity.findViewById(android.R.id.content);
-        mChildOfContent = content.getChildAt(0);
-
-        //界面出现变动都会调用这个监听事件
-        mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            public void onGlobalLayout() {
-                if (isfirst) {
-                    contentHeight = mChildOfContent.getHeight();//兼容华为等机型
-                    isfirst = false;
+    private AndroidBug5497Workaround(View content) {
+        if (content != null) {
+            mChildOfContent = content;
+            mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                public void onGlobalLayout() {
+                    possiblyResizeChildOfContent();
                 }
-                possiblyResizeChildOfContent();
-            }
-        });
-
-        frameLayoutParams = (FrameLayout.LayoutParams)
-                mChildOfContent.getLayoutParams();
+            });
+            frameLayoutParams = mChildOfContent.getLayoutParams();
+        }
     }
 
-    //重新调整跟布局的高度
     private void possiblyResizeChildOfContent() {
-
         int usableHeightNow = computeUsableHeight();
-
-        //当前可见高度和上一次可见高度不一致 布局变动
         if (usableHeightNow != usableHeightPrevious) {
-            //int usableHeightSansKeyboard2 = mChildOfContent.getHeight();//兼容华为等机型
-            int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
-            int heightDifference = usableHeightSansKeyboard - usableHeightNow;
-            if (heightDifference > (usableHeightSansKeyboard / 4)) {
-                // keyboard probably just became visible
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
-                    //frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
-                    frameLayoutParams.height = usableHeightSansKeyboard - heightDifference + statusBarHeight;
-                } else {
-                    frameLayoutParams.height = usableHeightSansKeyboard -heightDifference;
-                }
-            } else {
-                frameLayoutParams.height = contentHeight;
-            }
-
-            mChildOfContent.requestLayout();
+            //如果两次高度不一致
+            //将计算的可视高度设置成视图的高度
+            frameLayoutParams.height = usableHeightNow;
+            mChildOfContent.requestLayout();//请求重新布局
             usableHeightPrevious = usableHeightNow;
         }
     }
-    /**     * 计算mChildOfContent可见高度     ** @return     */
+
     private int computeUsableHeight() {
+        //计算视图可视高度
         Rect r = new Rect();
         mChildOfContent.getWindowVisibleDisplayFrame(r);
-        return (r.bottom - r.top);
+        return (r.bottom);
     }
 }
