56ff5dfdf99cfc844d032fc02b540862274210ef
zxy <zhangxyg@qq.com>
2021-06-22 17:58:53 +0800

    Update 导入Glide v4.10.0源码 Demo

--- app/src/main/java/com/zxy/demo/MainActivity.java
+++ app/src/main/java/com/zxy/demo/MainActivity.java
@@ -1,14 +1,19 @@
 package com.zxy.demo;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
-import com.obelieve.frame.base.ApiBaseActivity2;
-import com.zxy.demo.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 
-public class MainActivity extends ApiBaseActivity2<ActivityMainBinding> {
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void initCreateAfterView(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView iv = findViewById(R.id.iv);
+        Glide.with(this).load("https://pic.netbian.com/uploads/allimg/180826/113958-1535254798fc1c.jpg").into(iv);
     }
 }
